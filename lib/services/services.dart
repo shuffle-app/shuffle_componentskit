60bf2b05a51cc2a19d4db12221b0390d44f2398a
romancores <225000@niuitmo.ru>
2023-04-21 15:44:22 -0500

    dartcodemetrics

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -3,3 +3,4 @@ export 'configuration/configuration_loader.dart';
 export 'issue_handler/simple_issue_hadler.dart';
 export 'configuration/data/config_constants.dart';
 export 'issue_handler/navigation_service/navigation_key.dart';
+export 'issue_handler/widget_issue_catcher.dart';
