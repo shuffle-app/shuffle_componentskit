eeae8c2c6046c99f1d6a371ab0e4530635c5a12d
romancores <225000@niuitmo.ru>
2023-05-01 09:58:19 -0500

    - SHF-24 - SHF-45

--- lib/presentation/components/mood/mood_ui_model.dart
+++ lib/presentation/components/mood/mood_ui_model.dart
@@ -2,10 +2,11 @@ import '../../../shuffle_components_kit.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 class UiKitMood {
+  final String id;
   final String title;
   final String logo;
   final List<DescriptionItem>? descriptionItems;
   final List<UiKitPlace>? places;
 
-  UiKitMood({required this.title, required this.logo, this.descriptionItems, this.places});
+  UiKitMood({required this.id, required this.title, required this.logo, this.descriptionItems, this.places});
 }
