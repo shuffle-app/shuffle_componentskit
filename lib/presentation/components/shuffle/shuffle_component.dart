902a966e1af35694f03e74aa75700ae34f29a8c3
romancores <225000@niuitmo.ru>
2023-06-24 13:41:14 -0500

    bugfix

--- lib/presentation/components/shuffle/shuffle_component.dart
+++ lib/presentation/components/shuffle/shuffle_component.dart
@@ -16,13 +16,12 @@ class ShuffleComponent extends StatefulWidget {
   final Function? onFavorite;
   final Function? onCardTap;
 
-  const ShuffleComponent(
-      {Key? key,
-      required this.shuffle,
-      this.onLike,
-      this.onDislike,
-      this.onCardTap,
-      this.onFavorite})
+  const ShuffleComponent({Key? key,
+    required this.shuffle,
+    this.onLike,
+    this.onDislike,
+    this.onCardTap,
+    this.onFavorite})
       : super(key: key);
 
   @override
@@ -46,7 +45,11 @@ class _ShuffleComponentState extends State<ShuffleComponent> {
   @override
   void initState() {
     final config =
-        GlobalComponent.of(context)?.globalConfiguration.appConfig.content ??
+        GlobalComponent
+            .of(context)
+            ?.globalConfiguration
+            .appConfig
+            .content ??
             GlobalConfiguration().appConfig.content;
     model = ComponentShuffleModel.fromJson(config['shuffle']);
     unawaited(_getColor(widget.shuffle.items.first.imageLink ?? ''));
@@ -58,18 +61,25 @@ class _ShuffleComponentState extends State<ShuffleComponent> {
     if (imageLink.isEmpty) return;
     late final PaletteGenerator paletteGenerator;
     if (imageLink.substring(0, 4) == 'http') {
-      final file = await CustomCacheManager.instance.getFileStream(imageLink).firstWhere((element) => element is FileInfo);
-      paletteGenerator = await PaletteGenerator.fromImageProvider(Image.file((file as FileInfo).file).image);
+      final file = await CustomCacheManager.instance.getFileStream(imageLink)
+          .firstWhere((element) => element is FileInfo);
+      paletteGenerator = await PaletteGenerator.fromImageProvider(Image
+          .file((file as FileInfo).file)
+          .image);
     } else {
-      paletteGenerator = await PaletteGenerator.fromImageProvider(Image.asset(
+      paletteGenerator = await PaletteGenerator.fromImageProvider(Image
+          .asset(
         imageLink,
         package: 'shuffle_uikit',
-      ).image);
+      )
+          .image);
     }
     setState(() {
-      final dominantColor = paletteGenerator.dominantColor?.color ?? Colors.black12;
+      final dominantColor = paletteGenerator.dominantColor?.color ??
+          Colors.black12;
 
-      _backgroundColor = HSLColor.fromColor(dominantColor).withLightness(0.45).toColor();
+      _backgroundColor =
+          HSLColor.fromColor(dominantColor).withLightness(0.45).toColor();
       currentImage = ImageWidget(
         key: UniqueKey(),
         link: imageLink,
@@ -81,7 +91,7 @@ class _ShuffleComponentState extends State<ShuffleComponent> {
   }
 
   _onEnd() {
-    setState((){
+    setState(() {
       isEnded = true;
     });
   }
@@ -119,40 +129,29 @@ class _ShuffleComponentState extends State<ShuffleComponent> {
               crossAxisAlignment: bodyAlignment.crossAxisAlignment,
               children: [
                 Text('Try\nyourself',
-                        style: theme?.boldTextTheme.title1,
-                        textAlign: TextAlign.center)
+                    style: theme?.boldTextTheme.title1,
+                    textAlign: TextAlign.center)
                     .paddingSymmetric(
-                        vertical: SpacingFoundation.verticalSpacing12),
+                    vertical: SpacingFoundation.verticalSpacing12),
                 SizedBox(
                   height: 1.sh / 1.6,
                   width: 1.sw - 24,
-                  child: GestureDetector(
-                      onTap: widget.onCardTap == null
-                          ? null
-                          : () => widget.onCardTap!(
-                              widget.shuffle.items[controller.state?.index ?? 0]
-                                  .title,
-                              widget.shuffle.items[controller.state?.index ?? 0]
-                                  .imageLink),
-                      child: Stack(fit: StackFit.passthrough, children: [
-                        AnimatedScale(
-                          scale: isEnded ? 1 : 0.3,
-                          duration: animDuration,
-                          child: UiKitLastSwiperCard(),
-                        ),
-                        if (!isEnded)
-                          UiKitCardSwiper(
+                  child: Stack(fit: StackFit.passthrough, children: [
+                    AnimatedScale(
+                      scale: isEnded ? 1 : 0.3,
+                      duration: animDuration,
+                      child: UiKitLastSwiperCard(),
+                    ),
+                    if (!isEnded)
+                      UiKitCardSwiper(
                             // onEnd: _onEnd,
-                            onSwipe: (
-                              previousIndex,
-                              currentIndex,
-                              direction,
-                            ) {
+                            onSwipe: (previousIndex,
+                                currentIndex,
+                                direction,) {
                               if (currentIndex == null) return true;
                               if (direction != CardSwiperDirection.bottom) {
                                 _getColor(widget.shuffle
-                                        .items[currentIndex ?? 0].imageLink ??'');
-
+                                    .items[currentIndex ?? 0].imageLink ?? '');
                               }
 
                               switch (direction) {
@@ -190,7 +189,7 @@ class _ShuffleComponentState extends State<ShuffleComponent> {
                             cards: widget.shuffle.items,
                             controller: controller,
                           )
-                      ])),
+                  ]),
                 ),
                 SpacingFoundation.verticalSpace4,
                 AnimatedOpacity(
