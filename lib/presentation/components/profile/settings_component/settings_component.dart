f87cca88ad6d0e4571976afb7367e3389374f2a5
donik1998 <donikmurodkulov98@gmail.com>
2023-09-08 20:40:17 +0500

    added tab bar for settings_component.dart

--- lib/presentation/components/profile/settings_component/settings_component.dart
+++ lib/presentation/components/profile/settings_component/settings_component.dart
@@ -1,13 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:shuffle_components_kit/shuffle_components_kit.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
-import 'package:flutter/material.dart';
 
 class SettingsComponent extends StatelessWidget {
   final ValueChanged<String?> callback;
+  final ValueChanged<String>? onTabSwotched;
 
   const SettingsComponent({
     Key? key,
     required this.callback,
+    this.onTabSwotched,
   }) : super(key: key);
 
   @override
@@ -26,20 +28,17 @@ class SettingsComponent extends StatelessWidget {
     );
     const double iconWidht = 20.0;
 
-    final Map<String, PropertiesBaseModel> buttons =
-    Map.of(model.content.body?[ContentItemType.button]?.properties ?? {}).map((
-        key, value) => MapEntry(key.toUpperCase(), value))
+    final Map<String, PropertiesBaseModel> buttons = Map.of(model.content.body?[ContentItemType.button]?.properties ?? {})
+        .map((key, value) => MapEntry(key.toUpperCase(), value))
       ..removeWhere((key, value) => value.color != null);
 
-    final Map<String, PropertiesBaseModel> redButtons =
-    Map.of(model.content.body?[ContentItemType.button]?.properties ?? {}).map((
-        key, value) => MapEntry(key.toUpperCase(), value))
+    final Map<String, PropertiesBaseModel> redButtons = Map.of(model.content.body?[ContentItemType.button]?.properties ?? {})
+        .map((key, value) => MapEntry(key.toUpperCase(), value))
       ..removeWhere((key, value) => value.color == null);
 
     final List<BaseUiKitButtonData> btnDataList = [];
 
-    for (var i in buttons
-        .entries) {
+    for (var i in buttons.entries) {
       btnDataList.add(BaseUiKitButtonData(
           icon: ImageWidget(
             link: i.value.imageLink,
@@ -50,13 +49,11 @@ class SettingsComponent extends StatelessWidget {
           text: i.key,
           onPressed: () => callback.call(i.value.value)));
     }
-    btnDataList.sort((a, b) =>
-        buttons[a.text]!.sortNumber!.compareTo(buttons[b.text]!.sortNumber!));
+    btnDataList.sort((a, b) => buttons[a.text]!.sortNumber!.compareTo(buttons[b.text]!.sortNumber!));
 
     final List<BaseUiKitButtonData> redBtnDataList = [];
 
-    for (var i in redButtons
-        .entries) {
+    for (var i in redButtons.entries) {
       redBtnDataList.add(BaseUiKitButtonData(
           icon: ImageWidget(
             link: i.value.imageLink,
@@ -67,10 +64,9 @@ class SettingsComponent extends StatelessWidget {
           text: i.key,
           onPressed: () => callback.call(i.value.value)));
     }
-    redBtnDataList.sort((a, b) =>
-        redButtons[a.text]!
-            .sortNumber!
-            .compareTo(redButtons[b.text]!.sortNumber!));
+    redBtnDataList.sort((a, b) => redButtons[a.text]!.sortNumber!.compareTo(redButtons[b.text]!.sortNumber!));
+
+    final tabs = model.content.body?[ContentItemType.tabBar]?.properties ?? {};
 
     return Column(children: [
       if (model.content.title != null)
@@ -80,34 +76,36 @@ class SettingsComponent extends StatelessWidget {
             mainAxisAlignment: titleAligment.mainAxisAlignment,
             children: [
               Text(
-                model.content.title![ContentItemType.text]?.properties?.keys
-                    .first ??
-                    'Settings',
+                model.content.title![ContentItemType.text]?.properties?.keys.first ?? 'Settings',
                 style: textStyle?.title1,
               ),
               SpacingFoundation.verticalSpace12
             ]),
+      if (tabs.isNotEmpty) ...[
+        SpacingFoundation.verticalSpace16,
+        UiKitCustomTabBar(
+          onTappedTab: (index) => onTabSwotched?.call(tabs.keys.elementAt(index)),
+          tabs: tabs.keys.map((e) => UiKitCustomTab(title: e.toUpperCase())).toList(),
+        ),
+        SpacingFoundation.verticalSpace12,
+      ],
       Theme(
-          data: ThemeData(
-              textButtonTheme: TextButtonThemeData(
-                  style: context.uiKitTheme?.textButtonStyle)),
+          data: ThemeData(textButtonTheme: TextButtonThemeData(style: context.uiKitTheme?.textButtonStyle)),
           child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: bodyAligment.crossAxisAlignment,
               mainAxisAlignment: bodyAligment.mainAxisAlignment,
               children: btnDataList
                   .map(
-                    (e) =>
-                [context.button(isTextButton: true, data: e), divider],
-              )
+                    (e) => [context.button(isTextButton: true, data: e), divider],
+                  )
                   .expand((element) => element)
                   .toList())),
       Theme(
           data: ThemeData(
               textButtonTheme: TextButtonThemeData(
-                  style: context.uiKitTheme?.textButtonStyle.copyWith(
-                      foregroundColor: MaterialStateProperty.resolveWith(
-                              (states) => UiKitColors.error)))),
+                  style: context.uiKitTheme?.textButtonStyle
+                      .copyWith(foregroundColor: MaterialStateProperty.resolveWith((states) => UiKitColors.error)))),
           child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: bodyAligment.crossAxisAlignment,
