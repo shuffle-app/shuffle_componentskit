726bf918c2c255c7f20457adc60dd927ff854252
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-09-14 09:35:12 +0500

    sync with master

--- lib/presentation/components/place/uiplace_model.dart
+++ lib/presentation/components/place/uiplace_model.dart
@@ -35,15 +35,13 @@ class UiPlaceModel {
     required this.tags,
     this.baseTags = const [],
     this.weekdays = const [],
-  }):descriptionItems=[
-    UiDescriptionItemModel(title: 'Website', description: website ?? ''),
-    UiDescriptionItemModel(title: 'Phone', description: phone ?? ''),
-    UiDescriptionItemModel(
-        title: 'Location', description: location ?? ''),
-    UiDescriptionItemModel(
-      title: 'Work hours',
-      description:
-      '${openFrom!=null ? normalizedTi(openFrom) : 'nn'} - ${openTo!=null ? normalizedTi(openTo) : 'nn'}',
-    ),
-  ];
+  }) : descriptionItems = [
+          UiDescriptionItemModel(title: 'Website', description: website ?? ''),
+          UiDescriptionItemModel(title: 'Phone', description: phone ?? ''),
+          UiDescriptionItemModel(title: 'Location', description: location ?? ''),
+          UiDescriptionItemModel(
+            title: 'Work hours',
+            description: '${openFrom != null ? normalizedTi(openFrom) : 'nn'} - ${openTo != null ? normalizedTi(openTo) : 'nn'}',
+          ),
+        ];
 }
