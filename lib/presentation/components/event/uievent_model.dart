b2f5040f047a8b39c55d09c2849f20982b40b54b
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-22 12:47:45 +0300

    Updated UiEventModel

--- lib/presentation/components/event/uievent_model.dart
+++ lib/presentation/components/event/uievent_model.dart
@@ -47,7 +47,7 @@ class UiEventModel {
             UiDescriptionItemModel(
               title: 'Don’t miss it',
               description:
-                  '${(time != null ? '${normalizedTi(time)} ' : 'nn')}${timeTo != null ? ' - ${normalizedTi(timeTo)}, ' : 'nn,'}${DateFormat('MMM dd').format(date)}${dateTo != null ? ' - ${DateFormat('MMM dd, yyyy').format(dateTo)}' : ''}',
+                  '${(time != null ? normalizedTi(time, showDateName: false) : 'nn ')}${timeTo != null ? '- ${normalizedTi(timeTo, showDateName: false)}, ' : 'nn, '}${DateFormat('MMM dd').format(date)}${dateTo != null ? ' - ${DateFormat('MMM dd, yyyy').format(dateTo)}' : ''}',
             ),
           if (location != null)
             UiDescriptionItemModel(
