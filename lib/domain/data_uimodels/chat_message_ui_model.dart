ab441bc34edfa9c218c627685ba8c550f856825f
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-09-28 21:53:33 +0500

    implemented chat component

--- lib/domain/data_uimodels/chat_message_ui_model.dart
+++ lib/domain/data_uimodels/chat_message_ui_model.dart
@@ -1,14 +1,38 @@
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
 class ChatMessageUiModel {
   final DateTime timeSent;
-  final String message;
+  final String? message;
   final bool senderIsMe;
   final MessageType messageType;
+  final ChatMessageInvitationData? invitationData;
+
+  bool get isInvitation => messageType == MessageType.invitation && invitationData != null;
 
   ChatMessageUiModel({
     required this.timeSent,
-    required this.message,
+    this.message,
     required this.senderIsMe,
     required this.messageType,
+    this.invitationData,
+  });
+}
+
+class ChatMessageInvitationData {
+  final int placeId;
+  final String placeName;
+  final String placeImagePath;
+  final List<String?> invitedPeopleAvatarPaths;
+  final List<UiKitTag> tags;
+  final String username;
+
+  ChatMessageInvitationData({
+    required this.username,
+    required this.placeId,
+    required this.placeName,
+    required this.placeImagePath,
+    required this.invitedPeopleAvatarPaths,
+    required this.tags,
   });
 }
 
