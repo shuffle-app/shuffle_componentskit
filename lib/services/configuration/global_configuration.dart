f2c9c3304255b1f640e89a15061b27791dc4645a
romancores <225000@niuitmo.ru>
2023-10-23 11:53:50 -0500

    SHF-545

--- lib/services/configuration/global_configuration.dart
+++ lib/services/configuration/global_configuration.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -12,17 +13,21 @@ class GlobalConfiguration {
   static final GlobalConfiguration _singleton = GlobalConfiguration._internal();
   static String _configUrl = ConfigConstants.configAlternativeUrl;
   static int _timeout = 1;
+  static String? _languageCode;
   final Completer _completer = Completer();
   ConfigurationModel appConfig = ConfigurationModel(updated: DateTime.now(), content: {}, theme: 'default');
 
   bool get isLoaded => _completer.isCompleted;
 
-  factory GlobalConfiguration([String? configUrl, int? timeout]) {
+  factory GlobalConfiguration([String? configUrl, String? languageCode, int? timeout]) {
     if (configUrl != null) {
       _configUrl = configUrl;
     }
     if (timeout != null) {
       _timeout = timeout;
+    }
+    if (languageCode != null) {
+      _languageCode = languageCode;
     }
 
     return _singleton;
@@ -30,13 +35,13 @@ class GlobalConfiguration {
 
   GlobalConfiguration._internal();
 
-  Future<GlobalConfiguration> load({String version = '1.0.0', String? userId}) async {
+  Future<GlobalConfiguration> load({String version = '1.0.0', String? userId,bool forceUpdate = false}) async {
     File? cache = kIsWeb ? null : await _loadFromDocument();
     late final String cacheAsString;
     late ConfigurationModel model;
     if (cache != null && cache.existsSync()) {
       cacheAsString = await cache.readAsString();
-      if (cacheAsString.isNotEmpty) {
+      if (cacheAsString.isNotEmpty && !forceUpdate) {
         model = ConfigurationModel.fromJson(jsonDecode(cacheAsString));
         //TODO change to days
         // print('here is load from GlobalConfiguration model.updated.difference(DateTime.now()) ${model.updated.difference(DateTime.now())} ${model.updated.difference(DateTime.now()).inMinutes}');
@@ -60,6 +65,7 @@ class GlobalConfiguration {
       ConfigConstants.configHeaders.putIfAbsent('userId', () => userId);
     }
     Map<String, dynamic> configAsMap = (await _getFromUrl('http://${_configUrl}/api/v1/settings/config/$version',
+        queryParameters: _languageCode != null ? {'lang': _languageCode!} : null,
         headers: ConfigConstants.configHeaders))['config'];
     // print('got configAsMap $configAsMap');
     final model = ConfigurationModel(updated: DateTime.now(), content: configAsMap, theme: configAsMap['theme_name']);
@@ -97,6 +103,7 @@ class GlobalConfiguration {
     usableHeader.putIfAbsent('Accept', () => 'application/json');
 
     var encodedUri = Uri.encodeFull(finalUrl);
+    log('finalUrl for fetching config: $encodedUri', name: 'GlobalConfiguration|loadFromUrl');
     var response = await http.get(Uri.parse(encodedUri), headers: usableHeader);
     if (response.statusCode != 200) {
       throw Exception('HTTP request failed, statusCode: ${response.statusCode}, $finalUrl');
