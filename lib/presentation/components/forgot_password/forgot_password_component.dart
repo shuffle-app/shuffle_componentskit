8f35f39a8e52fa9d2af29f1dccf2d58a52074955
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-11-02 23:49:14 +0500

    fixing linter comments

--- lib/presentation/components/forgot_password/forgot_password_component.dart
+++ lib/presentation/components/forgot_password/forgot_password_component.dart
@@ -12,14 +12,9 @@ class ForgotPasswordDialogComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final boldTextTheme = context.uiKitTheme?.boldTextTheme;
-    final colorScheme = context.uiKitTheme?.colorScheme;
     final forgotPasswordConfig = ComponentModel.fromJson(GlobalConfiguration().appConfig.content['forgot_password']);
     final horizontalMargin = (forgotPasswordConfig.positionModel?.horizontalMargin ?? 0).toDouble();
     final verticalMargin = (forgotPasswordConfig.positionModel?.verticalMargin ?? 0).toDouble();
-    final title = forgotPasswordConfig.content.title?[ContentItemType.text]?.properties?.keys.first;
-    final subtitle = forgotPasswordConfig.content.subtitle?[ContentItemType.text]?.properties?.keys.first;
-    final button = forgotPasswordConfig.content.body?[ContentItemType.button]?.properties?.entries.first;
 
     return SafeArea(
       child: Dialog(
