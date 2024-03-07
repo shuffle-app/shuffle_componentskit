c302c603342521f907c60f525c6648809d601e12
romancores <225000@niuitmo.ru>
2024-03-07 13:17:20 -0500

    SHF-34

---
+++ lib/presentation/components/profile/public_profile/public_profile_component.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_components_kit/shuffle_components_kit.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+import '../../../../domain/config_models/profile/component_profile_model.dart';
+
+class PublicProfileComponent extends StatelessWidget {
+  final UiProfileModel uiProfileModel;
+  final ProfileStats? profileStats;
+
+  const PublicProfileComponent({super.key, required this.uiProfileModel, this.profileStats});
+
+  @override
+  Widget build(BuildContext context) {
+    final config =
+        GlobalComponent.of(context)?.globalConfiguration.appConfig.content ?? GlobalConfiguration().appConfig.content;
+    final ComponentProfileModel model = ComponentProfileModel.fromJson(config['profile']);
+    final horizontalMargin = (model.positionModel?.horizontalMargin ?? 0).toDouble();
+    final verticalMargin = (model.positionModel?.verticalMargin ?? 0).toDouble();
+
+    return BlurredAppBarPage(
+        centerTitle: true,
+        title: uiProfileModel.name ?? '',
+        autoImplyLeading: true,
+        bodyBottomSpace: verticalMargin,
+        children: [
+          verticalMargin.heightBox,
+          uiProfileModel.cardWidget.paddingSymmetric(horizontal: horizontalMargin),
+          if (profileStats != null)
+            ProfileHighlights(
+              placesVisited: profileStats!.placesVisited,
+              reviewsPosted: profileStats!.reviewsPosted,
+              points: profileStats!.points,
+            ).paddingSymmetric(horizontal: horizontalMargin),
+        ]);
+  }
+}
