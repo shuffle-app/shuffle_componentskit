52f726a0da25d1673bfbe451d483347a7fa30809
romancores <225000@niuitmo.ru>
2024-05-23 11:18:18 -0500

    SFK-183

--- lib/presentation/components/event/my_events_component.dart
+++ lib/presentation/components/event/my_events_component.dart
@@ -13,18 +13,19 @@ class MyEventsComponent extends StatelessWidget {
     required this.onEventTap,
     required this.onTap,
     required this.events,
+    this.buttonText,
   });
 
   final String title;
   final Function(UiEventModel) onEventTap;
   final VoidCallback onTap;
+  final String? buttonText;
   final List<UiEventModel> events;
 
   @override
   Widget build(BuildContext context) {
-    final height = (events.length >= 2 ? 110.h : max(events.length, 1) * 70.h) + 40.h;
+    final height = ((events.isNotEmpty ? events.length : 1) * 70.h) + 80.h;
     final theme = context.uiKitTheme;
-    debugPrint(events.map((e) => e.id).toString());
 
     return UiKitCardWrapper(
       height: height,
@@ -59,77 +60,83 @@ class MyEventsComponent extends StatelessWidget {
                 },
               ),
               const Spacer(),
-              if(events.isNotEmpty)
-              context.smallOutlinedButton(
-                  data: BaseUiKitButtonData(
-                      onPressed: onTap,
-                      iconInfo: BaseUiKitButtonIconData(
-                        iconData: CupertinoIcons.right_chevron,
-                        size: 20.w,
-                        color: theme?.colorScheme.inversePrimary,
-                      )))
+              // if (events.isNotEmpty)
+              //   context.smallOutlinedButton(
+              //       data: BaseUiKitButtonData(
+              //           onPressed: onTap,
+              //           iconInfo: BaseUiKitButtonIconData(
+              //             iconData: CupertinoIcons.right_chevron,
+              //             size: 20.w,
+              //             color: theme?.colorScheme.inversePrimary,
+              //           )))
             ],
           ),
           SpacingFoundation.verticalSpace16,
-          Expanded(
-            child: events.isEmpty
-                ? Text(
-                    S.of(context).CreateYourEventAndInvitePeople,
-                    style: theme?.boldTextTheme.subHeadline,
-                  )
-                : ListView.separated(
-                    padding: EdgeInsets.zero,
-                    itemCount: min(events.length, 2),
-                    itemBuilder: (context, index) {
-                      final event = events[index];
+          // Expanded(
+          //   child:
+          if (events.isEmpty)
+            Text(
+              S.of(context).CreateYourEventAndInvitePeople,
+              style: theme?.boldTextTheme.subHeadline,
+            )
+          else
+            // ListView.separated(
+            //                 padding: EdgeInsets.zero,
+            //                 physics: const NeverScrollableScrollPhysics(),
+            //                 itemCount: min(events.length, 2),
+            //                 itemBuilder: (context, index) {
+            for (var event in events)
+              // final event = events[index];
 
-                      return event.reviewStatus != null
-                          ? DecoratedBox(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadiusFoundation.all24,
-                                  gradient: LinearGradient(colors: [
-                                    theme!.colorScheme.inversePrimary.withOpacity(0.3),
-                                    Colors.transparent
-                                  ])),
-                              child: Center(
-                                child: Text(
-                                  event.reviewStatus!,
-                                  style: theme.boldTextTheme.caption1Bold.copyWith(color: Colors.white),
-                                ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing8),
-                              ))
-                          : ListTile(
-                              contentPadding: EdgeInsets.zero,
-                              leading: BorderedUserCircleAvatar(
-                                imageUrl:
-                                    event.media.firstWhere((element) => element.type == UiKitMediaType.image).link,
-                                size: 40.w,
+              event.reviewStatus != null
+                  ? DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadiusFoundation.all24,
+                          gradient: LinearGradient(
+                              colors: [theme!.colorScheme.inversePrimary.withOpacity(0.3), Colors.transparent])),
+                      child: Center(
+                        child: Text(
+                          event.reviewStatus!,
+                          style: theme.boldTextTheme.caption1Bold.copyWith(color: Colors.white),
+                        ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing8),
+                      )).paddingOnly(bottom: SpacingFoundation.verticalSpacing16)
+                  : ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: BorderedUserCircleAvatar(
+                        imageUrl: event.media.firstWhere((element) => element.type == UiKitMediaType.image).link,
+                        size: 40.w,
+                      ),
+                      title: Text(
+                        event.title ?? '',
+                        style: theme?.boldTextTheme.caption1Bold,
+                      ),
+                      //TODO restore schedules
+                      subtitle: event.date != null
+                          ? Text(
+                              DateFormat('MMMM d').format(event.date!),
+                              style: theme?.boldTextTheme.caption1Medium.copyWith(
+                                color: theme.colorScheme.darkNeutral500,
                               ),
-                              title: Text(
-                                event.title ?? '',
-                                style: theme?.boldTextTheme.caption1Bold,
-                              ),
-                              //TODO restore schedules
-                              subtitle: event.date != null
-                                  ? Text(
-                                      DateFormat('MMMM d').format(event.date!),
-                                      style: theme?.boldTextTheme.caption1Medium.copyWith(
-                                        color: theme.colorScheme.darkNeutral500,
-                                      ),
-                                    )
-                                  : const SizedBox.shrink(),
-                              trailing: context.smallButton(
-                                data: BaseUiKitButtonData(
-                                  onPressed: () => onEventTap(event),
-                                  iconInfo: BaseUiKitButtonIconData(
-                                    iconData: CupertinoIcons.right_chevron,
-                                    size: 20.w,
-                                  ),
-                                ),
-                              ),
-                            );
-                    },
-                    separatorBuilder: (_, __) => SpacingFoundation.verticalSpace4,
-                  ),
+                            )
+                          : const SizedBox.shrink(),
+                      trailing: context.smallButton(
+                        data: BaseUiKitButtonData(
+                          onPressed: () => onEventTap(event),
+                          iconInfo: BaseUiKitButtonIconData(
+                            iconData: CupertinoIcons.right_chevron,
+                            size: 20.w,
+                          ),
+                        ),
+                      ),
+                    ).paddingOnly(bottom: SpacingFoundation.verticalSpacing16),
+          // ;
+          //                 },
+          //                 separatorBuilder: (_, __) => SpacingFoundation.verticalSpace4,
+          //               ),
+          //       ),
+          context.gradientButton(
+            data: BaseUiKitButtonData(
+                onPressed: onTap, text: buttonText ?? S.of(context).CreateEvent, fit: ButtonFit.fitWidth),
           ),
         ],
       ).paddingAll(EdgeInsetsFoundation.all16),
