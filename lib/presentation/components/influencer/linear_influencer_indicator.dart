83aa4b60a3fbc26f67fc701018cbb8b924f19fdf
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-06-10 17:52:10 +0300

    fix

--- lib/presentation/components/influencer/linear_influencer_indicator.dart
+++ lib/presentation/components/influencer/linear_influencer_indicator.dart
@@ -1,43 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
-class LinearInfluencerIndicator extends StatefulWidget {
+class LinearInfluencerIndicator extends StatelessWidget {
   final int actualSum;
   final int sum;
   final double? width;
 
-  const LinearInfluencerIndicator({
+  LinearInfluencerIndicator({
     super.key,
     required this.actualSum,
     required this.sum,
     this.width,
   });
 
-  @override
-  State<LinearInfluencerIndicator> createState() => _LinearInfluencerIndicatorState();
-}
-
-class _LinearInfluencerIndicatorState extends State<LinearInfluencerIndicator> {
-  late double _progressPosition;
-  late final double _progressValue;
-  late final double _indicatorWidth;
+  late final double _indicatorWidth = width ?? 256.w;
   final double _rightIndicatorEdge = 4.w;
 
-  @override
-  void initState() {
-    super.initState();
-    _setPercentage();
-    _indicatorWidth = widget.width ?? 256.w;
-    _setPositionWithPercentage();
-  }
-
-  void _setPercentage() {
-    _progressValue = ((widget.actualSum / widget.sum) * 100);
-  }
-
-  void _setPositionWithPercentage() {
-    _progressPosition = _indicatorWidth * (_progressValue / 100);
-  }
+  double get _progressPosition => _indicatorWidth * (_progressValue / 100);
+  double get _progressValue => ((actualSum / sum) * 100);
 
   double _getCurrentPosition(double currentPosition) {
     if (currentPosition > _indicatorWidth - _rightIndicatorEdge) {
