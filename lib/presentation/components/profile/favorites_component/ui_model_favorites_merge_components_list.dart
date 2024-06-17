3e6e78a181db5faec7facdb830e22eb64adfc44d
ParaDox-PDT <109426212+ParaDox-PDT@users.noreply.github.com>
2024-06-17 19:21:39 +0500

    Task SFK 175 (#320)

--- lib/presentation/components/profile/favorites_component/ui_model_favorites_merge_components_list.dart
+++ lib/presentation/components/profile/favorites_component/ui_model_favorites_merge_components_list.dart
@@ -1,8 +1,8 @@
-import 'package:shuffle_components_kit/presentation/components/profile/favorites_component/ui_model_favorites_merge_component.dart';
+import 'package:shuffle_components_kit/domain/data_uimodels/content_short_ui_model.dart';
 
 class UiModelFavoritesMergeContentList {
   final String title;
-  final List<UiModelFavoritesMergeComponent> contents;
+  final List<ContentShortUiModel> contents;
 
   UiModelFavoritesMergeContentList({
     required this.title,
