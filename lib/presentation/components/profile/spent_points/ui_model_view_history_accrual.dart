3b3ce6a6c73b5da15c418049a11724fe307b10cf
ParaDox-PDT <109426212+ParaDox-PDT@users.noreply.github.com>
2024-06-20 00:27:13 +0500

    Merge pull request #317
    * Task SFK 240
    
    * Merge branch 'master' of https://github.com/shuffle-app/shuffle_compo…
    
    * Task SFK 240 updated
    
    * Merge branch 'master' of https://github.com/shuffle-app/shuffle_compo…
    
    * Merge branch 'master' of https://github.com/shuffle-app/shuffle_compo…
    
    * Task SFK 240 conflict fixed
    
    * Merge branch 'master' of https://github.com/shuffle-app/shuffle_compo…
    
    * Task SFK 238
    
    * Task SFK 240
    
    * Merge branch 'master' of https://github.com/shuffle-app/shuffle_compo…
    
    * Task SFK 240 bugs fixed

---
+++ lib/presentation/components/profile/spent_points/ui_model_view_history_accrual.dart
@@ -0,0 +1,11 @@
+class UiModelViewHistoryAccrual{
+  final String title;
+  final DateTime date;
+  final int points;
+
+  UiModelViewHistoryAccrual({
+    required this.title,
+    required this.date,
+    required this.points
+  });
+}
