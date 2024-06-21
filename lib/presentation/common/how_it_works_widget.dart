8998684be1fecf11a6ce9db013b7e8a38bbf73e1
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-06-21 15:50:21 +0300

    fix

--- lib/presentation/common/how_it_works_widget.dart
+++ lib/presentation/common/how_it_works_widget.dart
@@ -57,7 +57,7 @@ class HowItWorksWidget extends StatelessWidget {
     // final
 
     return Transform.rotate(
-      angle: pi * -20 / 180,
+      angle: pi * -10 / 180,
       child: RotatableWidget(
         animDuration: const Duration(milliseconds: 150),
         endAngle: pi * 20 / 180,
@@ -80,125 +80,125 @@ class HowItWorksWidget extends StatelessWidget {
   }
 }
 
-class CustomTextWithWidget extends MultiChildRenderObjectWidget {
-  final Text text;
+// class CustomTextWithWidget extends MultiChildRenderObjectWidget {
+//   final Text text;
 
-  CustomTextWithWidget({required this.text, required Widget widget})
-      : super(children: [text, widget]);
+//   CustomTextWithWidget({required this.text, required Widget widget})
+//       : super(children: [text, widget]);
 
-  @override
-  RenderObject createRenderObject(BuildContext context) {
-    return RenderCustomTextWithWidget(
-        textDirection: Directionality.of(context));
-  }
+//   @override
+//   RenderObject createRenderObject(BuildContext context) {
+//     return RenderCustomTextWithWidget(
+//         textDirection: Directionality.of(context));
+//   }
 
-  @override
-  void updateRenderObject(
-      BuildContext context, covariant RenderCustomTextWithWidget renderObject) {
-    renderObject.textDirection = Directionality.of(context);
-  }
-}
+//   @override
+//   void updateRenderObject(
+//       BuildContext context, covariant RenderCustomTextWithWidget renderObject) {
+//     renderObject.textDirection = Directionality.of(context);
+//   }
+// }
 
-class RenderCustomTextWithWidget extends RenderBox
-    with
-        ContainerRenderObjectMixin<RenderBox, CustomTextWithWidgetParentData>,
-        RenderBoxContainerDefaultsMixin<RenderBox,
-            CustomTextWithWidgetParentData> {
-  RenderCustomTextWithWidget({
-    required TextDirection textDirection,
-  })  : _textDirection = textDirection,
-        _textPainter = TextPainter(textDirection: textDirection);
+// class RenderCustomTextWithWidget extends RenderBox
+//     with
+//         ContainerRenderObjectMixin<RenderBox, CustomTextWithWidgetParentData>,
+//         RenderBoxContainerDefaultsMixin<RenderBox,
+//             CustomTextWithWidgetParentData> {
+//   RenderCustomTextWithWidget({
+//     required TextDirection textDirection,
+//   })  : _textDirection = textDirection,
+//         _textPainter = TextPainter(textDirection: textDirection);
 
-  final TextPainter _textPainter;
-  TextDirection _textDirection;
+//   final TextPainter _textPainter;
+//   TextDirection _textDirection;
 
-  TextDirection get textDirection => _textDirection;
-  set textDirection(TextDirection value) {
-    if (_textDirection != value) {
-      _textDirection = value;
-      _textPainter.textDirection = value;
-      markNeedsLayout();
-    }
-  }
+//   TextDirection get textDirection => _textDirection;
+//   set textDirection(TextDirection value) {
+//     if (_textDirection != value) {
+//       _textDirection = value;
+//       _textPainter.textDirection = value;
+//       markNeedsLayout();
+//     }
+//   }
 
-  @override
-  void setupParentData(RenderBox child) {
-    if (child.parentData is! CustomTextWithWidgetParentData) {
-      child.parentData = CustomTextWithWidgetParentData();
-    }
-  }
+//   @override
+//   void setupParentData(RenderBox child) {
+//     if (child.parentData is! CustomTextWithWidgetParentData) {
+//       child.parentData = CustomTextWithWidgetParentData();
+//     }
+//   }
 
-  @override
-  void performLayout() {
-    RenderBox? textBox = firstChild;
-    RenderBox? widgetBox = childAfter(textBox!);
+//   @override
+//   void performLayout() {
+//     RenderBox? textBox = firstChild;
+//     RenderBox? widgetBox = childAfter(textBox!);
 
-    textBox.layout(constraints, parentUsesSize: true);
-    final textParentData = textBox.parentData as CustomTextWithWidgetParentData;
-    textParentData.offset = Offset.zero;
+//     textBox.layout(constraints, parentUsesSize: true);
+//     final textParentData = textBox.parentData as CustomTextWithWidgetParentData;
+//     textParentData.offset = Offset.zero;
 
-    //TODO KOSTL
-    if (widgetBox != null) {
-      developer.log('textBox.size.heinght!!! ${textBox.size.height}');
-      developer.log('heinght ${54.h}');
+//     //TODO KOSTL
+//     if (widgetBox != null) {
+//       developer.log('textBox.size.heinght!!! ${textBox.size.height}');
+//       developer.log('heinght ${0.08.sh}');
 
-      developer.log('textBox.size.width ${textBox.size.width}');
-      developer.log('1.sw ${1.sw - SpacingFoundation.horizontalSpacing32}');
+//       // developer.log('textBox.size.width ${textBox.size.width}');
+//       // developer.log('1.sw ${1.sw - SpacingFoundation.horizontalSpacing32}');
 
-      widgetBox.layout(constraints.loosen(), parentUsesSize: true);
-      final widgetParentData =
-          widgetBox.parentData as CustomTextWithWidgetParentData;
-      //TODO KOSTL
-      widgetParentData.offset = Offset(
-          textBox.size.width != (1.sw - SpacingFoundation.horizontalSpacing32)
-              ? textBox.size.width
-              : textBox.size.width - 45.w,
-          textBox.size.height != 54.h ? 0 : 20);
-    }
+//       widgetBox.layout(constraints.loosen(), parentUsesSize: true);
+//       final widgetParentData =
+//           widgetBox.parentData as CustomTextWithWidgetParentData;
+//       //TODO KOSTL
+//       widgetParentData.offset = Offset(
+//           textBox.size.width != (1.sw - SpacingFoundation.horizontalSpacing32)
+//               ? textBox.size.width
+//               : textBox.size.width - 45.w,
+//           textBox.size.height <= 0.08.sh ? 0 : 0.03.sh);
+//     }
 
-    size = constraints.constrain(Size(
-      textBox.size.width + (widgetBox?.size.width ?? 0),
-      max(textBox.size.height, widgetBox?.size.height ?? 0),
-    ));
-  }
+//     size = constraints.constrain(Size(
+//       textBox.size.width + (widgetBox?.size.width ?? 0),
+//       max(textBox.size.height, widgetBox?.size.height ?? 0),
+//     ));
+//   }
 
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    RenderBox? textBox = firstChild;
-    RenderBox? widgetBox = childAfter(textBox!);
+//   @override
+//   void paint(PaintingContext context, Offset offset) {
+//     RenderBox? textBox = firstChild;
+//     RenderBox? widgetBox = childAfter(textBox!);
 
-    final textParentData = textBox.parentData as CustomTextWithWidgetParentData;
-    context.paintChild(textBox, offset + textParentData.offset);
+//     final textParentData = textBox.parentData as CustomTextWithWidgetParentData;
+//     context.paintChild(textBox, offset + textParentData.offset);
 
-    if (widgetBox != null) {
-      final widgetParentData =
-          widgetBox.parentData as CustomTextWithWidgetParentData;
-      context.paintChild(widgetBox, offset + widgetParentData.offset);
-    }
-  }
+//     if (widgetBox != null) {
+//       final widgetParentData =
+//           widgetBox.parentData as CustomTextWithWidgetParentData;
+//       context.paintChild(widgetBox, offset + widgetParentData.offset);
+//     }
+//   }
 
-  @override
-  bool hitTestChildren(BoxHitTestResult result, {required Offset position}) {
-    RenderBox? child = lastChild;
-    while (child != null) {
-      final CustomTextWithWidgetParentData childParentData =
-          child.parentData as CustomTextWithWidgetParentData;
-      if (result.addWithPaintOffset(
-        offset: childParentData.offset,
-        position: position,
-        hitTest: (BoxHitTestResult result, Offset transformed) {
-          assert(transformed == position - childParentData.offset);
-          return child!.hitTest(result, position: transformed);
-        },
-      )) {
-        return true;
-      }
-      child = childParentData.previousSibling;
-    }
+//   @override
+//   bool hitTestChildren(BoxHitTestResult result, {required Offset position}) {
+//     RenderBox? child = lastChild;
+//     while (child != null) {
+//       final CustomTextWithWidgetParentData childParentData =
+//           child.parentData as CustomTextWithWidgetParentData;
+//       if (result.addWithPaintOffset(
+//         offset: childParentData.offset,
+//         position: position,
+//         hitTest: (BoxHitTestResult result, Offset transformed) {
+//           assert(transformed == position - childParentData.offset);
+//           return child!.hitTest(result, position: transformed);
+//         },
+//       )) {
+//         return true;
+//       }
+//       child = childParentData.previousSibling;
+//     }
 
-    return false;
-  }
-}
+//     return false;
+//   }
+// }
 
-class CustomTextWithWidgetParentData
-    extends ContainerBoxParentData<RenderBox> {}
+// class CustomTextWithWidgetParentData
+//     extends ContainerBoxParentData<RenderBox> {}
