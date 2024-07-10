0197bc7a6ad43ea244f06cda934533e95124e101
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-10 11:36:19 +0300

    fix

--- lib/presentation/components/profile/points_component/ui_points_model.dart
+++ lib/presentation/components/profile/points_component/ui_points_model.dart
@@ -1,15 +1,15 @@
 class UiPointsModel {
-  final String title;
+  final String? title;
   final int getPoints;
   final double actualSum;
   final double sum;
-  final String imagePath;
+  final String? imagePath;
 
   UiPointsModel({
-    required this.title,
-    required this.getPoints,
-    required this.actualSum,
-    required this.sum,
-    required this.imagePath,
+    this.title,
+    this.getPoints = 0,
+    this.actualSum = 0,
+    this.sum = 1,
+    this.imagePath,
   });
 }
