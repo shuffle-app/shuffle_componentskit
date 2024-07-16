6cacc23029006898dc125be6110b0fde3e59107d
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-16 22:59:04 +0300

    feat: add UiUserPointsProgressBarModel

---
+++ lib/presentation/components/profile/points_component/ui_user_points_progress_bar_model.dart
@@ -0,0 +1,11 @@
+class UiUserPointsProgressBarModel {
+  final double actual;
+  final double sum;
+  final int level;
+
+  UiUserPointsProgressBarModel({
+    this.actual = 0,
+    this.sum = 100,
+    this.level = 0,
+  });
+}
