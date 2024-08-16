a31a000e3b3f64f59e92a9855679e1064dc75769
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-08-16 22:07:45 +0300

    feat: add UnderDevelopment if item is empty

--- lib/presentation/components/profile/spent_points/view_history_point_component.dart
+++ lib/presentation/components/profile/spent_points/view_history_point_component.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_components_kit/domain/data_uimodels/point_history_universal_model.dart';
-import 'package:shuffle_components_kit/presentation/components/components.dart';
 import 'package:shuffle_components_kit/presentation/presentation.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
-class ViewHistoryPointComponent extends StatelessWidget {
+class ViewHistoryPointComponent extends StatefulWidget {
   const ViewHistoryPointComponent({
     super.key,
     this.onTapBarCode,
@@ -18,6 +17,13 @@ class ViewHistoryPointComponent extends StatelessWidget {
   final PagingController<int, PointHistoryUniversalModel> pagingController;
 
   @override
+  State<ViewHistoryPointComponent> createState() => _ViewHistoryPointComponentState();
+}
+
+class _ViewHistoryPointComponentState extends State<ViewHistoryPointComponent> {
+  int tabValue = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: BlurredAppBarPage(
@@ -28,7 +34,10 @@ class ViewHistoryPointComponent extends StatelessWidget {
       children: [
         UiKitCustomTabBar(
           onTappedTab: (value) {
-            onTabChange?.call(value);
+            // widget.onTabChange?.call(value);
+            setState(() {
+              tabValue = value;
+            });
           },
           tabs: [
             UiKitCustomTab(
@@ -43,21 +52,39 @@ class ViewHistoryPointComponent extends StatelessWidget {
           height: 0.75.sh,
           child: PagedListView<int, PointHistoryUniversalModel>(
             padding: EdgeInsets.symmetric(vertical: EdgeInsetsFoundation.vertical8),
-            pagingController: pagingController,
+            pagingController: widget.pagingController,
             builderDelegate: PagedChildBuilderDelegate(
               noItemsFoundIndicatorBuilder: (_) => const UnderDevelopment(),
               itemBuilder: (context, item, index) {
-                return item.contentShortUiModel != null
-                    ? ViewHistoryActivationWidget(
-                        onTap: onTapBarCode,
-                        activationModel: item.contentShortUiModel,
-                      )
-                    : UiKitPointsHistoryTile(
-                        isLast: index == pagingController.itemList!.length - 1,
-                        title: item.uiModelViewHistoryAccrual?.title ?? '',
-                        points: item.uiModelViewHistoryAccrual?.points ?? 0,
-                        dateTime: item.uiModelViewHistoryAccrual?.date ?? DateTime.now(),
-                      );
+                if (item.contentShortUiModel != null && tabValue == 0) {
+                  return ViewHistoryActivationWidget(
+                    onTap: widget.onTapBarCode,
+                    activationModel: item.contentShortUiModel,
+                  );
+                } else if (item.uiModelViewHistoryAccrual != null && tabValue == 1) {
+                  return UiKitPointsHistoryTile(
+                    isLast: index == widget.pagingController.itemList!.length - 1,
+                    title: item.uiModelViewHistoryAccrual?.title ?? '',
+                    points: item.uiModelViewHistoryAccrual?.points ?? 0,
+                    dateTime: item.uiModelViewHistoryAccrual?.date ?? DateTime.now(),
+                  );
+                }
+                if (index == 0) {
+                  return const UnderDevelopment();
+                } else {
+                  return SpacingFoundation.none;
+                }
+                // return item.contentShortUiModel != null
+                //     ? ViewHistoryActivationWidget(
+                //         onTap: onTapBarCode,
+                //         activationModel: item.contentShortUiModel,
+                //       )
+                //     : UiKitPointsHistoryTile(
+                //         isLast: index == pagingController.itemList!.length - 1,
+                //         title: item.uiModelViewHistoryAccrual?.title ?? '',
+                //         points: item.uiModelViewHistoryAccrual?.points ?? 0,
+                //         dateTime: item.uiModelViewHistoryAccrual?.date ?? DateTime.now(),
+                //       );
               },
             ),
           ),
