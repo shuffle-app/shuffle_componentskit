01bea5dac604d3e769ed125b0cb50134d8d9c01c
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-08-21 11:56:01 +0300

    fix: routing and change to StatelessWidget

--- lib/presentation/components/bookings_control/bookings_control_component.dart
+++ lib/presentation/components/bookings_control/bookings_control_component.dart
@@ -4,40 +4,21 @@ import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 import 'bookings_control_event_item.dart';
 
-class BookingsControlComponent extends StatefulWidget {
-  final BookingUiModel? bookingUiModel;
+class BookingsControlComponent extends StatelessWidget {
   final List<BookingsEventUiModel>? events;
   final List<BookingsPlaceOrEventUiModel>? places;
-  final ValueChanged<int>? fullRefund;
-  final ValueChanged<int>? partialRefund;
-  final ValueChanged<int>? contactByMessage;
-  final ValueChanged<String?>? contactByEmail;
-  final ValueChanged<BookingUiModel>? onBookingEdit;
-  final ValueChanged<List<UserBookingsControlUiModel>?>? refundEveryone;
-  final VoidCallback? onGoAheadTap;
-  final ValueChanged<int>? onContactTap;
+  final ValueChanged<BookingsPlaceOrEventUiModel?>? onPlaceItemTap;
+  final ValueChanged<BookingsPlaceOrEventUiModel?>? onEventItemTap;
 
-  const BookingsControlComponent({
+  BookingsControlComponent({
     super.key,
     this.places,
     this.events,
-    this.fullRefund,
-    this.partialRefund,
-    this.refundEveryone,
-    this.contactByMessage,
-    this.contactByEmail,
-    this.bookingUiModel,
-    this.onBookingEdit,
-    this.onContactTap,
-    this.onGoAheadTap,
+    this.onPlaceItemTap,
+    this.onEventItemTap,
   });
 
-  @override
-  State<BookingsControlComponent> createState() => _BookingsControlComponentState();
-}
-
-class _BookingsControlComponentState extends State<BookingsControlComponent> {
-  String selectedTab = 'place';
+  final ValueNotifier<bool> tabChange = ValueNotifier(true);
 
   @override
   Widget build(BuildContext context) {
@@ -52,98 +33,70 @@ class _BookingsControlComponentState extends State<BookingsControlComponent> {
         children: [
           SpacingFoundation.verticalSpace16,
           UiKitCustomTabBar(
-            selectedTab: selectedTab,
             onTappedTab: (index) {
-              setState(() {
-                selectedTab = [
-                  'place',
-                  'events',
-                ][index];
-              });
+              if (index == 0) {
+                tabChange.value = true;
+              } else {
+                tabChange.value = false;
+              }
             },
             tabs: [
               UiKitCustomTab(
                 title: S.of(context).Place.toUpperCase(),
-                customValue: 'place',
               ),
               UiKitCustomTab(
                 title: S.of(context).Events.toUpperCase(),
-                customValue: 'events',
               ),
             ],
           ),
           SpacingFoundation.verticalSpace24,
-          if (selectedTab == 'place') ...[
-            if (widget.places != null && widget.places!.isNotEmpty) ...[
-              ListView.separated(
-                itemCount: widget.places!.length,
-                shrinkWrap: true,
-                padding: EdgeInsets.zero,
-                controller: ScrollController(),
-                separatorBuilder: (context, index) => SpacingFoundation.verticalSpace16,
-                itemBuilder: (context, index) {
-                  final element = widget.places![index];
+          ValueListenableBuilder(
+            valueListenable: tabChange,
+            builder: (context, value, child) {
+              return tabChange.value == true
+                  ? ListView.separated(
+                      itemCount: places!.length,
+                      shrinkWrap: true,
+                      padding: EdgeInsets.zero,
+                      controller: ScrollController(),
+                      separatorBuilder: (context, index) => SpacingFoundation.verticalSpace16,
+                      itemBuilder: (context, index) {
+                        final element = places![index];
 
-                  return BookingsControlPlaceItemUiKit(
-                    title: element.title,
-                    description: element.description,
-                    imageUrl: element.imageUrl,
-                    onTap: () => context.push(
-                      BookingsControlListComponent(
-                        fullRefund: widget.fullRefund,
-                        partialRefund: widget.partialRefund,
-                        refundEveryone: widget.refundEveryone,
-                        contactByEmail: widget.contactByEmail,
-                        contactByMessage: widget.contactByMessage,
-                        bookingsPlaceItemUiModel: element,
-                        bookingUiModel: widget.bookingUiModel,
-                        onBookingEdit: widget.onBookingEdit,
-                        onGoAheadTap: widget.onGoAheadTap,
-                        onContactTap: widget.onContactTap,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ]
-          ] else ...[
-            if (widget.events != null && widget.events!.isNotEmpty) ...[
-              ListView.separated(
-                itemCount: widget.events!.length,
-                shrinkWrap: true,
-                padding: EdgeInsets.zero,
-                controller: ScrollController(),
-                separatorBuilder: (context, index) => Divider(
-                  color: theme?.colorScheme.surface2,
-                  thickness: 2.0,
-                ).paddingOnly(bottom: SpacingFoundation.verticalSpacing16),
-                itemBuilder: (context, index) {
-                  final element = widget.events![index];
+                        return BookingsControlPlaceItemUiKit(
+                          title: element.title,
+                          description: element.description,
+                          imageUrl: element.imageUrl,
+                          onTap: () => onPlaceItemTap?.call(element),
+                        );
+                      },
+                    )
+                  : ListView.separated(
+                      itemCount: events!.length,
+                      shrinkWrap: true,
+                      padding: EdgeInsets.zero,
+                      controller: ScrollController(),
+                      separatorBuilder: (context, index) => Divider(
+                        color: theme?.colorScheme.surface2,
+                        thickness: 2.0,
+                      ).paddingOnly(bottom: SpacingFoundation.verticalSpacing16),
+                      itemBuilder: (context, index) {
+                        final element = events![index];
 
-                  return BookingsControlEventUiKit(
-                    title: element.title,
-                    description: element.description,
-                    events: element.events,
-                    onTap: (id) => context.push(
-                      BookingsControlListComponent(
-                        fullRefund: widget.fullRefund,
-                        partialRefund: widget.partialRefund,
-                        refundEveryone: widget.refundEveryone,
-                        contactByEmail: widget.contactByEmail,
-                        contactByMessage: widget.contactByMessage,
-                        bookingsPlaceItemUiModel: element.events?.firstWhere((element) => element.id == id),
-                        bookingUiModel: widget.bookingUiModel,
-                        onBookingEdit: widget.onBookingEdit,
-                        onGoAheadTap: widget.onGoAheadTap,
-                        onContactTap: widget.onContactTap,
-                      ),
-                    ),
-                  );
-                },
-              )
-            ]
-          ],
-          SpacingFoundation.verticalSpace24,
+                        return BookingsControlEventUiKit(
+                          title: element.title,
+                          description: element.description,
+                          events: element.events,
+                          onTap: (id) {
+                            onEventItemTap?.call(
+                              element.events?.firstWhere((element) => element.id == id),
+                            );
+                          },
+                        );
+                      },
+                    );
+            },
+          ),
         ],
       ),
     );
