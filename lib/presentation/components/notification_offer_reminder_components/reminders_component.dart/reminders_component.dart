00eaf9618a9dabcfdf768b1fca8db1740df7f5bb
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-09-17 15:59:19 +0300

    fix: remove chore

--- lib/presentation/components/notification_offer_reminder_components/reminders_component.dart/reminders_component.dart
+++ lib/presentation/components/notification_offer_reminder_components/reminders_component.dart/reminders_component.dart
@@ -8,39 +8,29 @@ class RemindersComponent extends StatelessWidget {
   final ValueChanged<int?>? onEditReminder;
   final ValueChanged<int?>? onRemoveReminder;
   final VoidCallback? onCreateReminder;
-  late final GlobalKey<SliverAnimatedListState> _listKey;
+  final GlobalKey<SliverAnimatedListState> listKey;
 
-  RemindersComponent({
+  const RemindersComponent({
     super.key,
+    required this.listKey,
     this.placeOrEventName,
     this.listReminders,
     this.onEditReminder,
     this.onRemoveReminder,
     this.onCreateReminder,
-  }) {
-    _listKey = GlobalKey<SliverAnimatedListState>();
-  }
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: UniversalFormForNotOfferRem(
-        animatedListKey: _listKey,
+        animatedListKey: listKey,
         title: S.of(context).Reminders,
         itemList: listReminders,
         nameForEmptyList: placeOrEventName,
         onCreateItem: onCreateReminder,
         onEditItem: onEditReminder,
-        onRemoveItem: (index) {
-          onRemoveReminder?.call(index);
-
-          if (index != null) {
-            _listKey.currentState?.removeItem(
-              index,
-              (context, animation) => SizeTransition(sizeFactor: animation),
-            );
-          }
-        },
+        onRemoveItem: onRemoveReminder,
         whatCreate: S.of(context).Reminder.toLowerCase(),
       ),
     );
