34ef87006532150edceeab8736942899a520feba
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-09-24 14:56:26 +0300

    feat: add isPromotion, PromotionItemWidget

--- lib/presentation/components/notification_offer_reminder_components/universal_form_for_not_offer_rem.dart
+++ lib/presentation/components/notification_offer_reminder_components/universal_form_for_not_offer_rem.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shuffle_components_kit/presentation/components/components.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
@@ -12,9 +13,11 @@ class UniversalFormForNotOfferRem extends StatelessWidget {
   final ValueChanged<int?>? onRemoveItem;
   final VoidCallback? onCreateItem;
   final GlobalKey<SliverAnimatedListState> animatedListKey;
+  final bool isPromotion;
 
   const UniversalFormForNotOfferRem({
     super.key,
+    required this.animatedListKey,
     this.nameForEmptyList,
     this.itemList,
     this.onEditItem,
@@ -22,7 +25,7 @@ class UniversalFormForNotOfferRem extends StatelessWidget {
     this.onCreateItem,
     this.title,
     this.whatCreate,
-    required this.animatedListKey,
+    this.isPromotion = false,
   });
 
   @override
@@ -81,18 +84,101 @@ class UniversalFormForNotOfferRem extends StatelessWidget {
           ).paddingOnly(top: SpacingFoundation.verticalSpacing16);
         } else {
           if (index < itemList!.length) {
-            return UniversalNotOfferRemItemWidget(
-              universalNotOfferRemUiModel: itemList?[index],
-              onDismissed: () => onRemoveItem?.call(itemList?[index]?.id),
-              onTap: () => onEditItem?.call(itemList?[index]?.id),
-            ).paddingOnly(
-              bottom: itemList?[index] != itemList?.last ? SpacingFoundation.verticalSpacing16 : 0,
-              top: itemList?[index] != itemList?.first ? 0 : SpacingFoundation.verticalSpacing16,
-            );
+            return isPromotion
+                ? PromotionItemWidget().paddingOnly(
+                    bottom: itemList?[index] != itemList?.last ? SpacingFoundation.verticalSpacing16 : 0,
+                    top: itemList?[index] != itemList?.first ? 0 : SpacingFoundation.verticalSpacing16,
+                  )
+                : UniversalNotOfferRemItemWidget(
+                    universalNotOfferRemUiModel: itemList?[index],
+                    onDismissed: () => onRemoveItem?.call(itemList?[index]?.id),
+                    onTap: () => onEditItem?.call(itemList?[index]?.id),
+                  ).paddingOnly(
+                    bottom: itemList?[index] != itemList?.last ? SpacingFoundation.verticalSpacing16 : 0,
+                    top: itemList?[index] != itemList?.first ? 0 : SpacingFoundation.verticalSpacing16,
+                  );
           }
         }
         return null;
       },
+    );
+  }
+}
+
+class PromotionItemWidget extends StatefulWidget {
+  final UniversalNotOfferRemUiModel? universalNotOfferRemUiModel;
+
+  const PromotionItemWidget({
+    super.key,
+    this.universalNotOfferRemUiModel,
+  });
+
+  @override
+  State<PromotionItemWidget> createState() => _PromotionItemWidgetState();
+}
+
+class _PromotionItemWidgetState extends State<PromotionItemWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.uiKitTheme;
+
+    return UiKitCardWrapper(
+      color: theme?.colorScheme.surface1,
+      child: Row(
+        children: [
+          context
+              .userAvatar(
+                size: UserAvatarSize.x40x40,
+                type: UserTileType.ordinary,
+                userName: '',
+                imageUrl: widget.universalNotOfferRemUiModel?.imagePath,
+              )
+              .paddingOnly(right: SpacingFoundation.horizontalSpacing8),
+          SpacingFoundation.horizontalSpace4,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.universalNotOfferRemUiModel?.title ?? S.of(context).NothingFound,
+                  style: theme?.boldTextTheme.caption1Bold,
+                ),
+                SpacingFoundation.verticalSpace2,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        widget.universalNotOfferRemUiModel?.selectedDates.toString() ?? S.of(context).NothingFound,
+                        style: theme?.boldTextTheme.caption1Medium.copyWith(color: ColorsFoundation.mutedText),
+                      ),
+                    ),
+                    SpacingFoundation.horizontalSpace12,
+                    ImageWidget(
+                      height: 10.h,
+                      fit: BoxFit.fill,
+                      color: ColorsFoundation.mutedText,
+                      link: widget.universalNotOfferRemUiModel?.isLaunched ?? true
+                          ? GraphicsFoundation.instance.svg.playOutline.path
+                          : GraphicsFoundation.instance.svg.stopOutline.path,
+                    ),
+                    SpacingFoundation.horizontalSpace2,
+                    Text(
+                      widget.universalNotOfferRemUiModel?.isLaunched ?? true
+                          ? S.of(context).Launched
+                          : S.of(context).Expired,
+                      style: theme?.boldTextTheme.caption1Medium.copyWith(color: ColorsFoundation.mutedText),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          SpacingFoundation.horizontalSpace24,
+          ImageWidget(
+            iconData: ShuffleUiKitIcons.chevrondown,
+          ),
+        ],
+      ).paddingAll(EdgeInsetsFoundation.all12),
     );
   }
 }
