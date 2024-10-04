89cdc3244f905d7c7c682ff4c86f0298ff66734d
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-04 13:26:23 +0300

    feat: add contentTitle, periodFrom and periodTo for ContentShortUiModel

--- lib/domain/data_uimodels/content_short_ui_model.dart
+++ lib/domain/data_uimodels/content_short_ui_model.dart
@@ -3,11 +3,17 @@ import 'package:shuffle_uikit/shuffle_uikit.dart';
 class ContentShortUiModel {
   final String? title;
   final String? imageUrl;
+  final String? contentTitle;
   final List<UiKitTag>? tags;
+  final DateTime? periodFrom;
+  final DateTime? periodTo;
 
   ContentShortUiModel({
     this.title,
     this.imageUrl,
+    this.contentTitle,
+    this.periodFrom,
+    this.periodTo,
     this.tags,
   });
 }
