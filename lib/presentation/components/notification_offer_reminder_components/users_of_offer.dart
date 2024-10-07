ea40bde0931261f54b518b1fc18b48ec10267e53
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-07 17:16:24 +0300

    feat: add UsersOfOffer class

---
+++ lib/presentation/components/notification_offer_reminder_components/users_of_offer.dart
@@ -0,0 +1,11 @@
+import 'package:shuffle_components_kit/presentation/components/profile/uiprofile_model.dart';
+
+class UsersOfOffer {
+  final UiProfileModel? user;
+  final bool isConfirm;
+
+  UsersOfOffer({
+    this.user,
+    this.isConfirm = false,
+  });
+}
