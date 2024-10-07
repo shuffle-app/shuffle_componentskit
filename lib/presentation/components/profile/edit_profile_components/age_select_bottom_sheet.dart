41394ed48d04a13c7d5039c1c0e1f9ab2fa3e256
romancores <225000@niuitmo.ru>
2024-10-07 16:15:50 +0400

    SFK-455

---
+++ lib/presentation/components/profile/edit_profile_components/age_select_bottom_sheet.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class AgeSelectBottomSheet extends StatelessWidget {
+  final ValueChanged<int> onAgeChanged;
+
+  const AgeSelectBottomSheet({super.key, required this.onAgeChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme  = context.uiKitTheme;
+    final boldTextTheme = theme?.boldTextTheme;
+    return SafeArea(
+        top: false,
+        child: Column(children: [
+          Text(
+            S.of(context).YourAge,
+            style: boldTextTheme?.labelLarge,
+          ).paddingOnly(
+            top: EdgeInsetsFoundation.vertical16,
+            left: EdgeInsetsFoundation.horizontal16,
+            right: EdgeInsetsFoundation.horizontal16,
+          ),
+          UiKitHorizontalWheelNumberSelector(
+            hideTitle: true,
+            values: List<int>.generate(70, (index) => index + 13),
+            backgroundColor: theme?.colorScheme.surface,
+            initialValue: 11,
+            onValueChanged: (age) => onAgeChanged(age),
+          )
+        ]));
+  }
+}
