7ee51761afea74fce1bb925dfca07427a7941ff1
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-07 17:13:26 +0300

    feat: add contentImageUrl, contentTitle, onConfirmTap, onCancelTap

--- lib/presentation/components/activation_offers_component.dart/offer_info.dart
+++ lib/presentation/components/activation_offers_component.dart/offer_info.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:shuffle_components_kit/presentation/components/activation_offers_component.dart/activation_offers_component.dart';
+import 'package:shuffle_components_kit/presentation/components/activation_offers_component.dart/offer_title_widget.dart';
+import 'package:shuffle_components_kit/presentation/components/activation_offers_component.dart/user_in_offer_widget.dart';
 import 'package:shuffle_components_kit/presentation/components/notification_offer_reminder_components/universal_not_offer_rem_ui_model.dart';
+import 'package:shuffle_components_kit/presentation/components/notification_offer_reminder_components/users_of_offer.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
-import '../profile/uiprofile_model.dart';
-
 class OfferInfo extends StatelessWidget {
-  final UiProfileModel? user;
+  final UsersOfOffer? offerUser;
   final UniversalNotOfferRemUiModel? offerUiModel;
-  final bool isConfirm;
+  final String? contentImageUrl;
+  final String? contentTitle;
+  final Function(int? userId, int? offerId)? onConfirmTap;
+  final Function(int? userId, int? offerId)? onCancelTap;
 
   const OfferInfo({
     super.key,
-    this.user,
+    this.offerUser,
     this.offerUiModel,
-    this.isConfirm = false,
+    this.contentImageUrl,
+    this.contentTitle,
+    this.onCancelTap,
+    this.onConfirmTap,
   });
 
   @override
@@ -24,13 +30,15 @@ class OfferInfo extends StatelessWidget {
 
     return Scaffold(
       body: BlurredAppBarPage(
+        //TODO - ?
+        customToolbarBaseHeight: 60.h,
         customTitle: Expanded(
           child: AutoSizeText(
-            'Инфо о предложении',
+            S.of(context).OfferInfo,
             style: theme?.boldTextTheme.title1,
             textAlign: TextAlign.center,
             wrapWords: false,
-            maxLines: 1,
+            maxLines: 2,
           ),
         ),
         expandTitle: true,
@@ -39,19 +47,18 @@ class OfferInfo extends StatelessWidget {
         childrenPadding: EdgeInsets.symmetric(horizontal: SpacingFoundation.horizontalSpacing16),
         children: [
           SpacingFoundation.verticalSpace16,
-          UserInOfferWidget(user: user),
+          UserInOfferWidget(offerUser: offerUser),
           Row(
             children: [
               context.userAvatar(
                 size: UserAvatarSize.x40x40,
                 type: UserTileType.ordinary,
                 userName: '',
-                //TODo
-                imageUrl: '',
+                imageUrl: contentImageUrl,
               ),
               SpacingFoundation.horizontalSpace16,
               Text(
-                'At.mosphere',
+                contentTitle ?? S.of(context).NothingFound,
                 style: theme?.boldTextTheme.body,
               ),
             ],
@@ -63,7 +70,10 @@ class OfferInfo extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Text(
-                '25.07.2024',
+                formatDateWithCustomPattern(
+                  'dd.MM.yyyy',
+                  (offerUiModel?.selectedDates?.last ?? DateTime.now()).toLocal(),
+                ),
                 style: theme?.boldTextTheme.caption3Medium.copyWith(color: ColorsFoundation.mutedText),
               ),
             ],
@@ -71,26 +81,24 @@ class OfferInfo extends StatelessWidget {
           SpacingFoundation.verticalSpace16,
           OfferTitleWidget(offerUiModel: offerUiModel),
           SpacingFoundation.verticalSpace24,
-          if (!isConfirm) ...[
+          if (!(offerUser?.isConfirm ?? false)) ...[
             context.gradientButton(
               data: BaseUiKitButtonData(
                 text: S.of(context).Confirm.toUpperCase(),
-                //TODO
-                onPressed: () {},
+                onPressed: () => onConfirmTap?.call(offerUser?.user?.id, offerUiModel?.id),
               ),
             ),
             SpacingFoundation.verticalSpace16,
             context.outlinedButton(
               data: BaseUiKitButtonData(
                 text: S.of(context).Cancel.toUpperCase(),
-                //TODO
-                onPressed: () {},
+                onPressed: () => onCancelTap?.call(offerUser?.user?.id, offerUiModel?.id),
               ),
             ),
           ] else
             Center(
               child: Text(
-                'Confirmed'.toUpperCase(),
+                S.of(context).Confirmed.toUpperCase(),
                 style: theme?.boldTextTheme.body,
               ),
             ),
