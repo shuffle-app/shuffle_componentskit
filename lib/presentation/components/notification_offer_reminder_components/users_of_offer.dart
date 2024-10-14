66fd2b57ea21a97a87e76ed4b63cfb1a90c2d1e0
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-14 20:55:45 +0300

    feat: add copyWith

--- lib/presentation/components/notification_offer_reminder_components/users_of_offer.dart
+++ lib/presentation/components/notification_offer_reminder_components/users_of_offer.dart
@@ -3,9 +3,23 @@ import 'package:shuffle_components_kit/presentation/components/profile/uiprofile
 class UsersOfOffer {
   final UiProfileModel? user;
   final bool isConfirm;
+  final String? ticketNumber;
 
   UsersOfOffer({
     this.user,
     this.isConfirm = false,
+    this.ticketNumber,
   });
+
+  UsersOfOffer copyWith({
+    UiProfileModel? user,
+    bool? isConfirm,
+    String? ticketNumber,
+  }) {
+    return UsersOfOffer(
+      user: user ?? this.user,
+      isConfirm: isConfirm ?? this.isConfirm,
+      ticketNumber: ticketNumber ?? this.ticketNumber,
+    );
+  }
 }
