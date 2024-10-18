102a49e53ce1102b54c11bf83a6a177e0844d792
romancores <225000@niuitmo.ru>
2024-10-18 12:42:06 -0500

    fixes SFK-507

--- lib/presentation/components/my_booking_component/my_booking_component.dart
+++ lib/presentation/components/my_booking_component/my_booking_component.dart
@@ -31,73 +31,71 @@ class MyBookingComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = context.uiKitTheme;
 
-    return Scaffold(
-      body: BlurredAppBarPage(
-        title: S.of(context).MyBooking,
-        centerTitle: true,
-        autoImplyLeading: true,
-        childrenPadding: EdgeInsets.symmetric(horizontal: SpacingFoundation.horizontalSpacing16),
-        children: [
-          SpacingFoundation.verticalSpace16,
-          Text(
-            S.of(context).Upcoming,
-            style: theme?.boldTextTheme.title2,
+    return BlurredAppBarPage(
+      title: S.of(context).MyBooking,
+      centerTitle: true,
+      autoImplyLeading: true,
+      childrenPadding: EdgeInsets.symmetric(horizontal: SpacingFoundation.horizontalSpacing16),
+      children: [
+        SpacingFoundation.verticalSpace16,
+        Text(
+          S.of(context).Upcoming,
+          style: theme?.boldTextTheme.title2,
+        ),
+        SpacingFoundation.verticalSpace16,
+        for (int index = 0; index < (myBookingUiModelUpcoming?.length ?? 0); index++)
+          MyBookingItem(
+            myBookingUiModel: myBookingUiModelUpcoming?[index],
+            onAlertCircleTap: () => onAlertCircleTap?.call(myBookingUiModelUpcoming?[index].id),
+            onBarCodeTap: onBarCodeTap,
+            onFullRefundTap: onFullRefundTap,
+            onLeaveFeedbackTap: onLeaveFeedbackTap,
+            onPartialRefundTap: onPartialRefundTap,
+          ).paddingOnly(
+            bottom: myBookingUiModelUpcoming?[index] != myBookingUiModelUpcoming?.last
+                ? SpacingFoundation.verticalSpacing32
+                : 0,
           ),
-          SpacingFoundation.verticalSpace16,
-          for (int index = 0; index < (myBookingUiModelUpcoming?.length ?? 0); index++)
-            MyBookingItem(
-              myBookingUiModel: myBookingUiModelUpcoming?[index],
-              onAlertCircleTap: () => onAlertCircleTap?.call(myBookingUiModelUpcoming?[index].id),
-              onBarCodeTap: onBarCodeTap,
-              onFullRefundTap: onFullRefundTap,
-              onLeaveFeedbackTap: onLeaveFeedbackTap,
-              onPartialRefundTap: onPartialRefundTap,
-            ).paddingOnly(
-              bottom: myBookingUiModelUpcoming?[index] != myBookingUiModelUpcoming?.last
-                  ? SpacingFoundation.verticalSpacing32
-                  : 0,
+        SizedBox(height: SpacingFoundation.verticalSpacing32),
+        Row(
+          children: [
+            Text(
+              S.of(context).Past,
+              style: theme?.boldTextTheme.title2,
             ),
-          SizedBox(height: SpacingFoundation.verticalSpacing32),
-          Row(
-            children: [
-              Text(
-                S.of(context).Past,
-                style: theme?.boldTextTheme.title2,
-              ),
-              SpacingFoundation.horizontalSpace16,
-              Builder(
-                builder: (context) => GestureDetector(
-                  onTap: () => showUiKitPopover(
-                    context,
-                    customMinHeight: 30.h,
-                    showButton: false,
-                    title: Text(
-                      S.of(context).YouCanStillRequestRefundYourComplaint,
-                      style: theme?.regularTextTheme.body.copyWith(color: Colors.black87),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  child: ImageWidget(
-                    iconData: ShuffleUiKitIcons.info,
-                    width: 20.w,
-                    color: theme?.colorScheme.darkNeutral900,
+            SpacingFoundation.horizontalSpace16,
+            Builder(
+              builder: (context) => GestureDetector(
+                onTap: () => showUiKitPopover(
+                  context,
+                  customMinHeight: 30.h,
+                  showButton: false,
+                  title: Text(
+                    S.of(context).YouCanStillRequestRefundYourComplaint,
+                    style: theme?.regularTextTheme.body.copyWith(color: Colors.black87),
+                    textAlign: TextAlign.left,
                   ),
                 ),
+                child: ImageWidget(
+                  iconData: ShuffleUiKitIcons.info,
+                  width: 20.w,
+                  color: theme?.colorScheme.darkNeutral900,
+                ),
               ),
-            ],
-          ),
-          SpacingFoundation.verticalSpace16,
-          for (int index = 0; index < (myBookingUiModelPast?.length ?? 0); index++)
-            MyBookingItem(
-              myBookingUiModel: myBookingUiModelPast?[index],
-              onAlertCircleTap: () => onAlertCircleTap?.call(myBookingUiModelPast?[index].id),
-              onBarCodeTap: onBarCodeTap,
-              onFullRefundTap: onFullRefundTap,
-              onLeaveFeedbackTap: onLeaveFeedbackTap,
-              onPartialRefundTap: onPartialRefundTap,
-            ).paddingOnly(bottom: SpacingFoundation.verticalSpacing32)
-        ],
-      ),
+            ),
+          ],
+        ),
+        SpacingFoundation.verticalSpace16,
+        for (int index = 0; index < (myBookingUiModelPast?.length ?? 0); index++)
+          MyBookingItem(
+            myBookingUiModel: myBookingUiModelPast?[index],
+            onAlertCircleTap: () => onAlertCircleTap?.call(myBookingUiModelPast?[index].id),
+            onBarCodeTap: onBarCodeTap,
+            onFullRefundTap: onFullRefundTap,
+            onLeaveFeedbackTap: onLeaveFeedbackTap,
+            onPartialRefundTap: onPartialRefundTap,
+          ).paddingOnly(bottom: SpacingFoundation.verticalSpacing32)
+      ],
     );
   }
 }
