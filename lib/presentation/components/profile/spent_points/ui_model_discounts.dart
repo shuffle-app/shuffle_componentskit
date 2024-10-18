867b2cbed38e758ae303b40c694121dcdcf7bfc1
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-18 19:35:36 +0300

    feat: add copyWith

--- lib/presentation/components/profile/spent_points/ui_model_discounts.dart
+++ lib/presentation/components/profile/spent_points/ui_model_discounts.dart
@@ -3,11 +3,23 @@ import 'package:shuffle_components_kit/domain/data_uimodels/content_short_ui_mod
 class UiModelDiscounts {
   final String buttonTitle;
   final String barcode;
-  final ContentShortUiModel contentShortUiModel;
+  final ContentShortUiModel? contentShortUiModel;
 
   UiModelDiscounts({
     required this.buttonTitle,
     required this.barcode,
-    required this.contentShortUiModel,
+    this.contentShortUiModel,
   });
+
+  UiModelDiscounts copyWith({
+    String? buttonTitle,
+    String? barcode,
+    ContentShortUiModel? contentShortUiModel,
+  }) {
+    return UiModelDiscounts(
+      buttonTitle: buttonTitle ?? this.buttonTitle,
+      barcode: barcode ?? this.barcode,
+      contentShortUiModel: contentShortUiModel ?? this.contentShortUiModel,
+    );
+  }
 }
