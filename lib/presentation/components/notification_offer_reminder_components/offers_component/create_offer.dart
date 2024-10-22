f25233b474d416f0b91a974fc5298fda20f77196
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-22 22:44:28 +0300

    feat: add offer icons list

--- lib/presentation/components/notification_offer_reminder_components/offers_component/create_offer.dart
+++ lib/presentation/components/notification_offer_reminder_components/offers_component/create_offer.dart
@@ -1,21 +1,24 @@
+// ignore_for_file: implementation_imports
+
 import 'package:flutter/material.dart';
 import 'package:shuffle_components_kit/presentation/components/notification_offer_reminder_components/universal_not_offer_rem_ui_model.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:collection/src/iterable_extensions.dart';
 
 class CreateOffer extends StatefulWidget {
   final UniversalNotOfferRemUiModel? offerUiModel;
   final ValueChanged<UniversalNotOfferRemUiModel>? onCreateOffer;
-  final String? defaultItemImagePath;
   final int? offerPrice;
   final DateTime? lastDate;
+  final Map<int, String>? iconsList;
 
   const CreateOffer({
     super.key,
     this.offerUiModel,
     this.onCreateOffer,
     this.offerPrice,
-    this.defaultItemImagePath,
     this.lastDate,
+    this.iconsList,
   });
 
   @override
@@ -36,7 +39,7 @@ class _CreateOfferState extends State<CreateOffer> {
   late bool _notifyTheAudience;
 
   late int? _selectedIconIndex;
-  final List<String> _iconList = [];
+  late List<String>? _iconsList;
 
   @override
   void initState() {
@@ -47,10 +50,18 @@ class _CreateOfferState extends State<CreateOffer> {
     _isLaunchedDate = widget.offerUiModel?.isLaunchedDate;
     _notifyTheAudience = widget.offerUiModel?.notifyTheAudience ?? true;
     _isLaunched = widget.offerUiModel?.isLaunched ?? true;
-    for (var element in GraphicsFoundation.instance.png.offer.values) {
-      _iconList.add(element.path);
-    }
-    _selectedIconIndex = _iconList.indexWhere((element) => element == (widget.offerUiModel?.iconPath ?? 0));
+    _iconsList = widget.iconsList?.values.toList() ?? [];
+    _iconsList?.firstWhereIndexedOrNull(
+      (index, element) {
+        if (element == widget.offerUiModel?.iconPath) {
+          _selectedIconIndex = index;
+          return true;
+        } else {
+          _selectedIconIndex = null;
+          return false;
+        }
+      },
+    );
     super.initState();
   }
 
@@ -65,18 +76,39 @@ class _CreateOfferState extends State<CreateOffer> {
     _isLaunchedDate = widget.offerUiModel?.isLaunchedDate;
     _notifyTheAudience = widget.offerUiModel?.notifyTheAudience ?? true;
     _isLaunched = widget.offerUiModel?.isLaunched ?? true;
-    for (var element in GraphicsFoundation.instance.png.offer.values) {
-      _iconList.add(element.path);
-    }
-    _selectedIconIndex = _iconList.indexWhere((element) => element == (widget.offerUiModel?.iconPath ?? 0));
+    _iconsList = widget.iconsList?.values.toList() ?? [];
+    _iconsList?.firstWhereIndexedOrNull(
+      (index, element) {
+        if (element == widget.offerUiModel?.iconPath) {
+          _selectedIconIndex = index;
+          return true;
+        } else {
+          _selectedIconIndex = null;
+          return false;
+        }
+      },
+    );
   }
 
   void _onSubmit() {
     if (_formKey.currentState != null && _formKey.currentState!.validate() && _selectedDates.first != null) {
+      late final String? iconPath;
+      late final int? iconId;
+
+      if (_selectedIconIndex != null) {
+        iconPath = _iconsList?[_selectedIconIndex!];
+        iconId = widget.iconsList!.keys.firstWhereOrNull(
+          (k) => widget.iconsList?[k] == _iconsList?[_selectedIconIndex!],
+        );
+      } else {
+        iconPath = null;
+        iconId = null;
+      }
+
       _offerUiModel = _offerUiModel.copyWith(
         title: _titleController.text.trim(),
-        iconPath: (_selectedIconIndex == -1 || _selectedIconIndex == null) ? null : _iconList[_selectedIconIndex!],
-        imagePath: widget.defaultItemImagePath,
+        iconId: iconId,
+        iconPath: iconPath,
         isLaunched: _isLaunched,
         notifyTheAudience: _notifyTheAudience,
         pointPrice: _pointController.text.isEmpty ? 0 : int.parse(_pointController.text.trim().replaceAll(' ', '')),
@@ -194,7 +226,7 @@ class _CreateOfferState extends State<CreateOffer> {
             ),
             SpacingFoundation.verticalSpace4,
             UiKitSelectedIconWidget(
-              iconList: _iconList,
+              iconList: _iconsList,
               onIconTap: (index) {
                 setState(() {
                   if (_selectedIconIndex != index) {
