5d6397769318c52090bd3939b3e394b883ff4eed
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-25 14:00:00 +0300

    fix: remove _buildScanWindow, remove initState, remove Scaffold, add fun scanWindow

--- lib/presentation/components/barcode_scan_component/barcode_scan_component.dart
+++ lib/presentation/components/barcode_scan_component/barcode_scan_component.dart
@@ -18,36 +18,14 @@ class CustomBarcodeScanner extends StatefulWidget {
 
 class _CustomBarcodeScannerState extends State<CustomBarcodeScanner> {
   final MobileScannerController controller = MobileScannerController();
-  late final Rect scanWindow;
   final width = 0.76.sw;
   final height = 0.5.sw;
 
-  Widget _buildScanWindow(Rect scanWindowRect) {
-    return ValueListenableBuilder(
-      valueListenable: controller,
-      builder: (context, value, child) {
-        if (!value.isInitialized || !value.isRunning || value.size.isEmpty) {
-          return const LoadingWidget();
-        } else if (value.error != null) {
-          return ScannerErrorWidget(error: value.error!);
-        }
-
-        return CustomPaint(
-          painter: ScannerOverlay(scanWindowRect),
-        );
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    final centerHeight = 1.sh / 2;
-    final diff = 1.sw <= 380 ? 0.2 : 0.15;
-    scanWindow = Rect.fromCenter(
-      center: Offset(1.sw / 2, centerHeight - centerHeight * diff),
-      width: width,
-      height: height - 0.05.sw,
+  Rect scanWindow(bool isUiWindow) {
+    return Rect.fromCenter(
+      center: isUiWindow ? Offset.zero : Offset(1.sw / 2, 1.sh / 2),
+      width: isUiWindow ? width : 0.7.sw,
+      height: isUiWindow ? height - 0.05.sw : 0.6.sh,
     );
   }
 
@@ -59,44 +37,46 @@ class _CustomBarcodeScannerState extends State<CustomBarcodeScanner> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: S.of(context).Scanner,
-        autoImplyLeading: true,
-      ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            MobileScanner(
-              scanWindow: scanWindow,
-              controller: controller,
-              onDetect: widget.onDetect,
-              errorBuilder: (context, error, child) {
-                return ScannerErrorWidget(error: error);
-              },
-            ),
-            _buildScanWindow(scanWindow),
-            Center(
-              child: SizedBox(
-                height: height,
-                child: LottieAnimation(
-                  lottiePath: GraphicsFoundation.instance.animations.lottie.codeScannerPink.path,
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            Center(
-              child: CustomPaint(
-                painter: CornerBorderPainter(),
-                child: SizedBox(
-                  width: width,
-                  height: height - 0.02.sw,
-                ),
-              ),
-            ),
-          ],
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        MobileScanner(
+          scanWindow: scanWindow(false),
+          controller: controller,
+          onDetect: widget.onDetect,
+          errorBuilder: (context, error, child) {
+            return ScannerErrorWidget(error: error);
+          },
         ),
-      ),
+        ValueListenableBuilder(
+          valueListenable: controller,
+          builder: (context, value, child) {
+            if (!value.isInitialized || !value.isRunning || value.size.isEmpty) {
+              return const LoadingWidget();
+            } else if (value.error != null) {
+              return ScannerErrorWidget(error: value.error!);
+            }
+
+            return CustomPaint(
+              painter: ScannerOverlay(scanWindow(true)),
+            );
+          },
+        ),
+        SizedBox(
+          height: height,
+          child: LottieAnimation(
+            lottiePath: GraphicsFoundation.instance.animations.lottie.codeScannerPink.path,
+            fit: BoxFit.fill,
+          ),
+        ),
+        CustomPaint(
+          painter: CornerBorderPainter(),
+          child: SizedBox(
+            width: width,
+            height: height - 0.02.sw,
+          ),
+        ),
+      ],
     );
   }
 }
