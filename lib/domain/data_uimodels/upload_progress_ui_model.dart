a8ef93931f5b326bc76332fa9956902fec1fb648
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-11-20 16:34:58 +0300

    feat: add title

--- lib/domain/data_uimodels/upload_progress_ui_model.dart
+++ lib/domain/data_uimodels/upload_progress_ui_model.dart
@@ -1,9 +1,11 @@
 class UploadProgressUiModel {
   final int currentValue;
   final int totalValue;
+  final String? title;
 
   UploadProgressUiModel({
     required this.currentValue,
     required this.totalValue,
+    this.title,
   });
 }
