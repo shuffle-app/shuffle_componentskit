d297f75d6d882f5ff13879b9ad3372603c984edb
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-01-23 11:39:37 +0300

    add onEdit, and translate

--- lib/domain/data_uimodels/feedback_ui_model.dart
+++ lib/domain/data_uimodels/feedback_ui_model.dart
@@ -16,24 +16,33 @@ class FeedbackUiModel {
   final int? placeId;
   final int? eventId;
   final VoidCallback? onTap;
+  final VoidCallback? onEdit;
+  final bool canEdit;
   final List<BaseUiKitMedia> media;
+  final ValueNotifier<bool>? showTranslateButton;
+  final ValueNotifier<String>? translateText;
 
-  FeedbackUiModel(
-      {required this.feedbackAuthorName,
-      required this.feedbackText,
-      required this.id,
-      this.helpfulForUser,
-      this.empty = false,
-      this.feedbackAuthorPhoto,
-      this.feedbackDateTime,
-      this.feedbackRating,
-      this.feedbackAuthorType,
-      this.helpfulCount,
-      this.feedbackAuthorId,
-      this.placeId,
-      this.eventId,
-      this.media = const [],
-      this.onTap});
+  FeedbackUiModel({
+    required this.feedbackAuthorName,
+    required this.feedbackText,
+    required this.id,
+    this.helpfulForUser,
+    this.empty = false,
+    this.feedbackAuthorPhoto,
+    this.feedbackDateTime,
+    this.feedbackRating,
+    this.feedbackAuthorType,
+    this.helpfulCount,
+    this.feedbackAuthorId,
+    this.placeId,
+    this.eventId,
+    this.media = const [],
+    this.onTap,
+    this.onEdit,
+    this.canEdit = false,
+    this.showTranslateButton,
+    this.translateText,
+  });
 
   factory FeedbackUiModel.empty() => FeedbackUiModel(
         id: 0,
@@ -43,35 +52,45 @@ class FeedbackUiModel {
         helpfulForUser: false,
       );
 
-  FeedbackUiModel copyWith(
-      {String? feedbackAuthorName,
-      String? feedbackAuthorPhoto,
-      DateTime? feedbackDateTime,
-      int? feedbackRating,
-      UserTileType? feedbackAuthorType,
-      int? helpfulCount,
-      bool? helpfulForUser,
-      int? feedbackAuthorId,
-      int? placeId,
-      int? eventId,
-      List<BaseUiKitMedia>? media,
-      VoidCallback? onTap}) {
+  FeedbackUiModel copyWith({
+    String? feedbackAuthorName,
+    String? feedbackAuthorPhoto,
+    DateTime? feedbackDateTime,
+    int? feedbackRating,
+    UserTileType? feedbackAuthorType,
+    int? helpfulCount,
+    bool? helpfulForUser,
+    int? feedbackAuthorId,
+    int? placeId,
+    int? eventId,
+    List<BaseUiKitMedia>? media,
+    VoidCallback? onTap,
+    VoidCallback? onEdit,
+    bool? canEdit,
+    ValueNotifier<bool>? showTranslateButton,
+    ValueNotifier<String>? translateText,
+  }) {
     return FeedbackUiModel(
-        feedbackAuthorName: feedbackAuthorName ?? this.feedbackAuthorName,
-        feedbackAuthorPhoto: feedbackAuthorPhoto ?? this.feedbackAuthorPhoto,
-        feedbackDateTime: feedbackDateTime ?? this.feedbackDateTime,
-        feedbackRating: feedbackRating ?? this.feedbackRating,
-        feedbackAuthorType: feedbackAuthorType ?? this.feedbackAuthorType,
-        helpfulCount: helpfulCount ?? this.helpfulCount,
-        helpfulForUser: helpfulForUser ?? this.helpfulForUser,
-        feedbackText: feedbackText,
-        empty: empty,
-        id: id,
-        feedbackAuthorId: feedbackAuthorId?? this.feedbackAuthorId,
-        media: media ?? this.media,
-        placeId: placeId ?? this.placeId,
-        eventId: eventId ?? this.eventId,
-        onTap: onTap ?? this.onTap);
+      feedbackAuthorName: feedbackAuthorName ?? this.feedbackAuthorName,
+      feedbackAuthorPhoto: feedbackAuthorPhoto ?? this.feedbackAuthorPhoto,
+      feedbackDateTime: feedbackDateTime ?? this.feedbackDateTime,
+      feedbackRating: feedbackRating ?? this.feedbackRating,
+      feedbackAuthorType: feedbackAuthorType ?? this.feedbackAuthorType,
+      helpfulCount: helpfulCount ?? this.helpfulCount,
+      helpfulForUser: helpfulForUser ?? this.helpfulForUser,
+      feedbackText: feedbackText,
+      empty: empty,
+      id: id,
+      feedbackAuthorId: feedbackAuthorId ?? this.feedbackAuthorId,
+      media: media ?? this.media,
+      placeId: placeId ?? this.placeId,
+      eventId: eventId ?? this.eventId,
+      onTap: onTap ?? this.onTap,
+      showTranslateButton: showTranslateButton ?? this.showTranslateButton,
+      translateText: translateText ?? this.translateText,
+      onEdit: onEdit ?? this.onEdit,
+      canEdit: canEdit ?? this.canEdit,
+    );
   }
 
   @override
