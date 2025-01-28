ec5b5ed42e7c557e238dbdc26c2e6b1369f0d77e
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-01-28 19:31:08 +0300

    refactor

--- lib/presentation/components/activity_component/activity_item_widget.dart
+++ lib/presentation/components/activity_component/activity_item_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_components_kit/presentation/components/activity_component/activity_ui_model.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class ActivityItemWidget extends StatelessWidget {
   final ActivityUiModel? activityUiModel;
@@ -15,101 +16,109 @@ class ActivityItemWidget extends StatelessWidget {
     final colorScheme = theme?.colorScheme;
     final boldTextTheme = theme?.boldTextTheme;
 
-    return UiKitCardWrapper(
-      borderRadius: BorderRadiusFoundation.all24r,
-      color: theme?.colorScheme.surface1,
-      child: Row(
-        children: [
-          UiKitCardWrapper(
-            borderRadius: BorderRadiusFoundation.all12,
-            width: 61.w,
-            height: 40.h,
-            child: ImageWidget(
-              link: activityUiModel?.imageUrl,
-              fit: BoxFit.fill,
-            ),
-          ).paddingOnly(
-            top: SpacingFoundation.verticalSpacing12,
-            bottom: SpacingFoundation.verticalSpacing12,
-            left: SpacingFoundation.horizontalSpacing12,
-          ),
-          SpacingFoundation.horizontalSpace8,
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (activityUiModel?.title != null && activityUiModel!.title!.isNotEmpty)
-                SizedBox(
-                  width: 150.w,
-                  child: Text(
-                    activityUiModel!.title!,
-                    style: theme?.boldTextTheme.caption3Medium,
+    return LayoutBuilder(
+      builder: (context, size) {
+        final calculatedHeight = size.maxWidth * 0.2463;
+        final sideInfoCardsWidth = size.maxWidth * 0.1597;
+
+        return Material(
+          borderRadius: BorderRadiusFoundation.all24r,
+          clipBehavior: Clip.hardEdge,
+          color: theme?.colorScheme.surface1,
+          child: Ink(
+            height: calculatedHeight,
+            child: Row(
+              children: [
+                SpacingFoundation.horizontalSpace12,
+                ClipRRect(
+                  borderRadius: BorderRadiusFoundation.all12,
+                  child: ImageWidget(
+                    link: activityUiModel?.imageUrl,
+                    fit: BoxFit.cover,
+                    width: 0.2.sw,
                   ),
-                ),
-              SpacingFoundation.verticalSpace2,
-              SizedBox(
-                height: 15.w,
-                width: 150.w,
-                child: ListView(
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    if (activityUiModel?.rating != null && activityUiModel?.rating != 0.0)
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ImageWidget(
-                            iconData: ShuffleUiKitIcons.starfill,
-                            color: colorScheme?.inverseSurface,
-                            height: 0.040625.sw,
-                            width: 0.040625.sw,
-                            fit: BoxFit.cover,
+                ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing14),
+                SpacingFoundation.horizontalSpace8,
+                if (activityUiModel?.title != null && activityUiModel!.title!.isNotEmpty)
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                            activityUiModel!.title!,
+                            style: theme?.boldTextTheme.caption3Medium,
                           ),
-                          SpacingFoundation.horizontalSpace2,
-                          Text(
-                            doubleFormat(activityUiModel!.rating!),
-                            style: boldTextTheme?.caption3Medium,
-                          ).paddingOnly(top: 2.w),
-                          SpacingFoundation.horizontalSpace12,
-                        ],
-                      ),
-                    if (activityUiModel?.tags != null && activityUiModel!.tags!.isNotEmpty)
-                      ...activityUiModel!.tags!.map(
-                        (e) => UiKitTagWidget(
-                          title: e.title,
-                          icon: e.icon,
-                        ).paddingOnly(right: SpacingFoundation.horizontalSpacing12),
-                      ),
-                  ],
-                ),
-              )
-            ],
-          ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing12),
-          if (activityUiModel?.activity != null && activityUiModel?.activity != 0)
-            UiKitCardWrapper(
-              color: colorScheme?.surface3,
-              borderRadius: BorderRadiusFoundation.zero,
-              width: 50.w,
-              height: 65.w,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ImageWidget(
-                    color: colorScheme?.inversePrimary,
-                    svgAsset: GraphicsFoundation.instance.svg.twoPeople,
+                        ),
+                        Expanded(
+                          child: ListView(
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              if (activityUiModel?.rating != null && activityUiModel?.rating != 0.0)
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ImageWidget(
+                                      iconData: ShuffleUiKitIcons.starfill,
+                                      color: colorScheme?.inverseSurface,
+                                      height: 0.040625.sw,
+                                      width: 0.040625.sw,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    SpacingFoundation.horizontalSpace2,
+                                    Text(
+                                      doubleFormat(activityUiModel!.rating!),
+                                      style: boldTextTheme?.caption3Medium,
+                                    ).paddingOnly(top: SpacingFoundation.horizontalSpacing2),
+                                    SpacingFoundation.horizontalSpace12,
+                                  ],
+                                ),
+                              if (activityUiModel?.tags != null && activityUiModel!.tags!.isNotEmpty)
+                                ...activityUiModel!.tags!.map(
+                                  (e) => UiKitTagWidget(
+                                    title: e.title,
+                                    icon: e.icon,
+                                  ).paddingOnly(right: SpacingFoundation.horizontalSpacing12),
+                                ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing10),
                   ),
-                  SpacingFoundation.verticalSpace4,
-                  Text(
-                    '${activityUiModel!.activity!}',
-                    style: boldTextTheme?.body,
+                SpacingFoundation.horizontalSpace12,
+                if (activityUiModel?.activity != null && activityUiModel?.activity != 0)
+                  Ink(
+                    width: sideInfoCardsWidth,
+                    color: theme?.colorScheme.surface3,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ImageWidget(
+                          color: colorScheme?.inversePrimary,
+                          svgAsset: GraphicsFoundation.instance.svg.twoPeople,
+                          fit: BoxFit.fitHeight,
+                        ),
+                        SpacingFoundation.verticalSpace4,
+                        AutoSizeText(
+                          '${activityUiModel!.activity!}',
+                          style: boldTextTheme?.body,
+                          maxLines: 1,
+                        ),
+                      ],
+                    ).paddingSymmetric(horizontal: SpacingFoundation.horizontalSpacing8),
                   ),
-                ],
-              ),
-            ).paddingOnly(left: SpacingFoundation.horizontalSpacing12)
-        ],
-      ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
