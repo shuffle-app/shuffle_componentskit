9468d7cd61f1afc76eb4da052860c2bee5db75d3
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-02-21 09:24:34 +0300

    feat: add RefresherItem

---
+++ lib/presentation/components/refresher_component/widgets/refresher_item.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_components_kit/presentation/components/refresher_component/ui_model/refresher_ui_model.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:shuffle_uikit/ui_kit/molecules/dismissible/ui_kit_secondary_background_dismissible.dart';
+
+class RefresherItem extends StatelessWidget {
+  final RefresherUiModel? item;
+  final VoidCallback? onEdit;
+  final VoidCallback? onDismissed;
+  final VoidCallback? onLongPress;
+  final bool isEditingMode;
+
+  const RefresherItem({
+    super.key,
+    this.item,
+    this.onEdit,
+    this.onDismissed,
+    this.isEditingMode = false,
+    this.onLongPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.uiKitTheme;
+
+    return GestureDetector(
+      onLongPress: onLongPress,
+      child: Dismissible(
+        key: GlobalKey(),
+        direction: DismissDirection.endToStart,
+        background: SpacingFoundation.none,
+        dismissThresholds: const {DismissDirection.endToStart: 0.6},
+        secondaryBackground: UiKitSecondaryBackgroundDismissible(),
+        confirmDismiss: (direction) async {
+          if (direction == DismissDirection.startToEnd) {
+            return false;
+          } else if (direction == DismissDirection.endToStart) {
+            return true;
+          }
+          return false;
+        },
+        onDismissed: (DismissDirection direction) {
+          if (direction == DismissDirection.endToStart) {
+            onDismissed?.call();
+          }
+        },
+        child: UiKitCardWrapper(
+          color: theme?.colorScheme.surface2,
+          borderRadius: BorderRadiusFoundation.all24r,
+          child: Stack(
+            children: [
+              Row(
+                children: [
+                  context.userAvatar(
+                    size: UserAvatarSize.x40x40,
+                    type: UserTileType.ordinary,
+                    imageUrl: item?.contentImage ?? GraphicsFoundation.instance.png.mockAvatar.path,
+                    userName: '',
+                  ),
+                  SpacingFoundation.horizontalSpace8,
+                  Flexible(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item?.contentTitle ?? '',
+                          style: theme?.boldTextTheme.caption1Bold,
+                        ),
+                        SpacingFoundation.verticalSpace4,
+                        Row(
+                          children: [
+                            ImageWidget(
+                              height: 10.h,
+                              fit: BoxFit.fill,
+                              color: ColorsFoundation.mutedText,
+                              iconData: item?.isLaunched ?? true
+                                  ? ShuffleUiKitIcons.playoutline
+                                  : ShuffleUiKitIcons.stopoutline,
+                            ),
+                            SpacingFoundation.horizontalSpace2,
+                            Expanded(
+                              child: Text(
+                                item?.isLaunched ?? true ? S.of(context).Launched : S.of(context).Expired,
+                                style:
+                                    theme?.regularTextTheme.caption4Regular.copyWith(color: ColorsFoundation.mutedText),
+                              ),
+                            ),
+                            SpacingFoundation.horizontalSpace2,
+                            Text(
+                              formatDateWithCustomPattern(
+                                'dd.MM.yyyy',
+                                (item?.isLaunchedDate ?? DateTime.now()).toLocal(),
+                              ),
+                              style:
+                                  theme?.regularTextTheme.caption4Regular.copyWith(color: ColorsFoundation.mutedText),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ).paddingAll(EdgeInsetsFoundation.all12),
+              if (isEditingMode)
+                Positioned.fill(
+                  child: ColoredBox(
+                    color: ColorsFoundation.neutral48,
+                    child: Center(
+                      child: context
+                          .button(
+                            data: BaseUiKitButtonData(
+                              borderColor: theme?.colorScheme.headingTypography,
+                              onPressed: onEdit,
+                              backgroundColor: theme?.colorScheme.surface3,
+                              iconInfo: BaseUiKitButtonIconData(
+                                iconPath: GraphicsFoundation.instance.svg.pencil.path,
+                                color: theme?.colorScheme.headingTypography,
+                              ),
+                            ),
+                          )
+                          .paddingSymmetric(vertical: SpacingFoundation.verticalSpacing4),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
