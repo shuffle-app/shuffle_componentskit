6d90cfd9f01b3880e7cec02a85b5ee707b486f0a
romancores <225000@niuitmo.ru>
2025-03-07 12:21:33 +0800

    THB

--- lib/presentation/common/price_selector_component.dart
+++ lib/presentation/common/price_selector_component.dart
@@ -50,7 +50,7 @@ class _PriceSelectorComponentState extends State<PriceSelectorComponent> {
 
   final _currencies = {
     'RUB': GraphicsFoundation.instance.svg.russia.path,
-    'BAT': GraphicsFoundation.instance.svg.thailand.path,
+    'THB': GraphicsFoundation.instance.svg.thailand.path,
     'USD': GraphicsFoundation.instance.svg.usFlag.path,
     // 'hi' : GraphicsFoundation.instance.svg.india.path,
     'AED': GraphicsFoundation.instance.svg.arabic.path,
