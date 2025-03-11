c995d90228b42c6bd1d0901deaac77abcf25a198
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-03-11 16:21:33 +0300

    add loading state

--- lib/presentation/components/news_component/create_news_component.dart
+++ lib/presentation/components/news_component/create_news_component.dart
@@ -4,11 +4,13 @@ import 'package:shuffle_uikit/shuffle_uikit.dart';
 class CreateNewsComponent extends StatelessWidget {
   final TextEditingController controller;
   final VoidCallback? onCreateTap;
+  final bool isLoading;
 
   const CreateNewsComponent({
     super.key,
     required this.controller,
     this.onCreateTap,
+    this.isLoading = false,
   });
 
   @override
@@ -16,46 +18,55 @@ class CreateNewsComponent extends StatelessWidget {
     final theme = context.uiKitTheme;
 
     return Scaffold(
-      bottomNavigationBar: SafeArea(
-        child: context
-            .gradientButton(
-              data: BaseUiKitButtonData(
-                text: S.of(context).Create.toUpperCase(),
-                onPressed: onCreateTap,
-              ),
-            )
-            .paddingOnly(
-              bottom: SpacingFoundation.verticalSpacing16,
-              right: SpacingFoundation.horizontalSpacing16,
-              left: SpacingFoundation.horizontalSpacing16,
+      bottomNavigationBar: isLoading
+          ? null
+          : SafeArea(
+              child: context
+                  .gradientButton(
+                    data: BaseUiKitButtonData(
+                      text: S.of(context).Create.toUpperCase(),
+                      onPressed: onCreateTap,
+                    ),
+                  )
+                  .paddingOnly(
+                    bottom: SpacingFoundation.verticalSpacing16,
+                    right: SpacingFoundation.horizontalSpacing16,
+                    left: SpacingFoundation.horizontalSpacing16,
+                  ),
             ),
-      ),
       body: BlurredAppBarPage(
         title: S.of(context).CreateNews,
         centerTitle: true,
         autoImplyLeading: true,
         children: [
-          UiKitCardWrapper(
-            color: theme?.colorScheme.surface1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SpacingFoundation.verticalSpace4,
-                Text(
-                  S.of(context).WhatsNew,
-                  style: theme?.boldTextTheme.labelLarge,
-                ).paddingOnly(left: SpacingFoundation.horizontalSpacing12),
-                SpacingFoundation.verticalSpace4,
-                UiKitSymbolsCounterInputField(
-                  controller: controller,
-                  enabled: true,
-                  obscureText: false,
-                  hintText: S.current.DescribeYourIssue.toUpperCase(),
-                  showSymbols: false,
-                ).paddingAll(EdgeInsetsFoundation.all4),
-              ],
-            ),
-          ).paddingAll(EdgeInsetsFoundation.all16)
+          isLoading
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator().paddingAll(EdgeInsetsFoundation.all32),
+                  ],
+                )
+              : UiKitCardWrapper(
+                  color: theme?.colorScheme.surface1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SpacingFoundation.verticalSpace4,
+                      Text(
+                        S.of(context).WhatsNew,
+                        style: theme?.boldTextTheme.labelLarge,
+                      ).paddingOnly(left: SpacingFoundation.horizontalSpacing12),
+                      SpacingFoundation.verticalSpace4,
+                      UiKitSymbolsCounterInputField(
+                        controller: controller,
+                        enabled: true,
+                        obscureText: false,
+                        hintText: S.current.DescribeYourIssue.toUpperCase(),
+                        showSymbols: false,
+                      ).paddingAll(EdgeInsetsFoundation.all4),
+                    ],
+                  ),
+                ).paddingAll(EdgeInsetsFoundation.all16)
         ],
       ),
     );
