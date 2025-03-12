45b7a2769fbe00b0672d91e290be5b734af534e6
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-03-12 16:27:27 +0300

    feat PromotionsComponent

---
+++ lib/presentation/components/promotions/promotions_component.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_components_kit/shuffle_components_kit.dart';
+import 'package:shuffle_uikit/localization/l10n.dart';
+
+class PromotionsComponent extends StatelessWidget {
+  final String? placeOrEventName;
+  final String? placeOrEventImage;
+  final List<UniversalNotOfferRemUiModel>? listPromotion;
+  final ValueChanged<int?>? onEditPromo;
+  final ValueChanged<int?>? onRemovePromo;
+  final VoidCallback? onCreatePromo;
+  final ValueChanged<int>? onActivateTap;
+  final GlobalKey<SliverAnimatedListState> listKey;
+
+  /// Need for empty [listPromotion]
+  final bool isPlace;
+  final String? typeOfContent;
+
+  const PromotionsComponent({
+    super.key,
+    required this.listKey,
+    this.placeOrEventName,
+    this.placeOrEventImage,
+    this.listPromotion,
+    this.onEditPromo,
+    this.onRemovePromo,
+    this.onCreatePromo,
+    this.onActivateTap,
+    this.isPlace = false,
+    this.typeOfContent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: UniversalFormForNotOfferRem(
+        animatedListKey: listKey,
+        title: S.of(context).Promotions,
+        itemList: listPromotion,
+        nameForEmptyList: placeOrEventName,
+        onCreateItem: onCreatePromo,
+        onEditItem: onEditPromo,
+        onRemoveItem: onRemovePromo,
+        onActivateTap: onActivateTap,
+        customWhatCreate: S.of(context).CreateAPromotionForYourXAndIncreaseAttendance(
+              '${isPlace ? S.of(context).PlacePromo.toLowerCase() : S.of(context).EventPromo.toLowerCase()} ${typeOfContent?.toLowerCase()} ${placeOrEventName}',
+            ),
+      ),
+    );
+  }
+}
